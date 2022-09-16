9d1be796156c23764917acb9409bd5c3e3ab16e8
Kirk Benell <kirk.benell@sparkfun.com>
2022-09-16 07:03:59 -0600

    ported over twist device

---
+++ src/Spark/spDevTwist.cpp
@@ -0,0 +1,87 @@
+/*
+ *
+ * QwiicDevBME280.cpp
+ *
+ *  Device object for the BME280 Qwiic device. 
+ *
+ * 
+ *
+ */
+#include "Arduino.h"
+
+#include "spDevTwist.h"
+
+// For type system testing
+spType spDevTwist::Type;
+//----------------------------------------------------------------------------------------------------------
+// Constructor
+//
+// Object constructor. Performs initialization of device values, including device identifiers (name, I2C address),
+// and managed properties.
+
+spDevTwist::spDevTwist(){
+
+	// Setup unique identifiers for this device and basic device object systems
+	spSetupDeviceIdent(DEVICE_NAME);
+
+}
+
+//----------------------------------------------------------------------------------------------------------
+// Static method used to determine if devices is connected before creating this object (if creating dynamically)
+bool spDevTwist::isConnected(spDevI2C& i2cDriver){
+
+ 	return i2cDriver.ping(QWIIC_TWIST_ADDR);
+
+}
+//----------------------------------------------------------------------------------------------------------
+// onInitialize()
+//
+// Called during the startup/initialization of the driver (after the constructor is called).
+//
+// Place to initialized the underlying device library/driver
+//
+bool spDevTwist::onInitialize(TwoWire& wirePort){
+
+	bool rc = TWIST::begin(wirePort);	
+
+	if(!rc)
+	   Serial.println("TWIST - begin failed");
+	
+	last_count=0;
+	return rc;
+}
+
+//----------------------------------------------------------------------------------------------------------
+// onPropertyUpdate()
+//
+// Called when the value of a managed property was updated. 
+//
+void spDevTwist::onPropertyUpdate(const char * propName){
+
+	//Serial.print("PROPERTY UPDATE: ");Serial.println(propName);
+	//save();
+}
+
+bool spDevTwist::loop(void){
+
+	// process events
+	if(TWIST::isClicked())
+		on_clicked.emit();
+
+	int tmp= TWIST::getCount();
+	if(tmp != last_count){
+		last_count = tmp;
+		on_twist.emit(last_count);
+	}
+
+
+	return false;
+}
+//----------------------------------------------------------------------------------------------------------
+// Register this class with the system, enabling this driver during system 
+// initialization and device discovery. 
+
+
+spRegisterDevice(spDevTwist);
+
+
---
+++ src/Spark/spDevTwist.h
@@ -0,0 +1,52 @@
+/*
+ *
+ * 
+ *
+ */
+
+#pragma once 
+
+#include "Arduino.h"
+
+#include "spDevice.h"
+#include "SparkFun_Qwiic_Twist_Arduino_Library.h"
+
+// What is the name used to ID this device?
+#define DEVICE_NAME  "TWIST";
+//----------------------------------------------------------------------------------------------------------
+// Define our class - note we are sub-classing from the Qwiic Library
+class spDevTwist : public spDevice, public TWIST {
+
+public:
+
+	spDevTwist();
+
+	// Static Interface - used by the system to determine if this device is 
+	// connected before the object is instantiated.
+	static  bool isConnected(spDevI2C& i2cDriver); 	
+	static const char * getDeviceName(){ return DEVICE_NAME; };	
+
+	// Method called to initialize the class
+	bool onInitialize(TwoWire &);   
+
+	// Called when a managed property is updated
+	void onPropertyUpdate(const char *);	
+
+
+	// Events
+	// Our output event
+	spSignalVoid on_clicked;
+	spSignalInt  on_twist;
+
+	// Type testing: 
+	// A static instance var - that is an object (can check instance pointer)
+	static spType Type;
+	spType* getType(void){ return &Type;}
+
+	bool loop(void);
+
+private:
+	int last_count = 0;
+
+};
+
