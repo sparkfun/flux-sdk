5e6a7cec9b39bb347447c8b3ec96f35c72c725ea
Kirk Benell <kirk.benell@sparkfun.com>
2022-09-30 17:35:39 -0600

    moved the get() calls to always find all of the type, and always return a vectory; for template typed git calls, made retunred vector typed to search value, so no more casting at the user level - nice!

--- examples/Example-04_Logger/Example-04_Logger.ino
+++ examples/Example-04_Logger/Example-04_Logger.ino
@@ -93,8 +93,6 @@ void setup() {
     // Loop over the device list - note that it is iterable. 
     for (auto device: myDevices )
     {
-        // TYPE ID TESTING
-        Serial.printf("TYPEID: %d %d\n\r", device->getType(), spDevBME280::type());
         // END TESTING
         Serial.printf("Device: %s, Output Number: %d", device->name(), device->nOutputParameters());
         if ( device->nOutputParameters() > 0)
@@ -108,18 +106,18 @@ void setup() {
 
     ////////////
     // getAll() testing
-    auto buttons = spark.getAll<spDevButton>();
+    auto allButtons = spark.get<spDevButton>();
 
-    Serial.printf("Number of buttons: %d \n\r", buttons->size());
-    for( auto b : *buttons)
+    Serial.printf("Number of buttons: %d \n\r", allButtons->size());
+    for( auto button: *allButtons)
     {
-        Serial.printf("Button Name: %s", b->name());
+        Serial.printf("Button Name: %s", button->name());
 
         // Have the button trigger a log entry
-        logger.listen(((spDevButton*)b)->on_clicked);
+        logger.listen(button->on_clicked);
         
         // Lets long the value of the button event
-        logger.listenLogEvent(((spDevButton*)b)->on_clicked, b);        
+        logger.listenLogEvent(button->on_clicked, button);        
     }
 
     /// END TESTING
