85de11e3ac64ee24190c71720d388bc1cd3b412c
Paul <pc235603@gmail.com>
2022-09-30 15:49:13 +0100

    Add ADS122C04 (Qwiic PT100)

---
+++ src/Spark/spDevADS122C04.cpp
@@ -0,0 +1,99 @@
+/*
+ *
+ *  spDevADS122C04.cpp
+ *
+ *  Spark Device object for the ADS122C04 A/D converter device.
+ * 
+ * 
+ */
+
+#include "Arduino.h"
+
+#include "spDevADS122C04.h"
+
+
+// The ADS122C04 supports multiple addresses. The SparkX breakout can be set to 0x45, 0x44, 0x41, 0x40
+uint8_t spDevADS122C04::defaultDeviceAddress[] = { 0x45, 0x44, 0x41, 0x40, kSparkDeviceAddressNull};
+
+// Register this class with the system - this enables the *auto load* of this device
+spRegisterDevice(spDevADS122C04);
+
+spDevADS122C04::spDevADS122C04()
+{
+
+    spSetupDeviceIdent(getDeviceName());
+
+    // Register Property
+    spRegister(wireMode, "Wire mode", "Wire mode: 0 = 4-wire, 1 = 3-wire, 2 = 2-wire");
+    wireMode = ADS122C04_4WIRE_MODE; // Default to 4-wire mode
+    spRegister(sampleRate, "Sample rate", "Sample rate: 0 = 20SPS, 6 = 1000SPS");
+    sampleRate = ADS122C04_DATA_RATE_20SPS; // Default to 20 samples per second
+
+    // Register output params
+    spRegister(temperatureC, "Probe temperature (C)", "Probe temperature (C)");
+    spRegister(temperatureF, "Probe temperature (F)", "Probe temperature (F)");
+    spRegister(internalTemperature, "Internal temperature (C)", "Internal temperature (C)");
+    spRegister(rawVoltage, "Raw voltage (V)", "Raw voltage (V)");
+
+}
+
+// Function to encapsulate the ops needed to get values from the sensor.
+float spDevADS122C04::read_temperature_c()
+{
+    return SFE_ADS122C04::readPT100Centigrade();
+}
+float spDevADS122C04::read_temperature_f()
+{
+    return SFE_ADS122C04::readPT100Fahrenheit();
+}
+float spDevADS122C04::read_internal_temperature()
+{
+    return SFE_ADS122C04::readInternalTemperature(sampleRate);
+}
+float spDevADS122C04::read_raw_voltage()
+{
+    int32_t raw_v = SFE_ADS122C04::readRawVoltage(sampleRate);
+    return (((float)raw_v) * 244.14e-9); // Convert to Volts
+}
+
+// Static method used to determine if this device is connected
+
+bool spDevADS122C04::isConnected(spDevI2C &i2cDriver, uint8_t address)
+{
+
+    return i2cDriver.ping(address);
+}
+
+//----------------------------------------------------------------------------------------------------------
+// onInitialize()
+//
+// Called during the startup/initialization of the driver (after the constructor is called).
+//
+// Place to initialized the underlying device library/driver
+//
+bool spDevADS122C04::onInitialize(TwoWire &wirePort)
+{
+	// set the underlying drivers address to the one determined during
+	// device construction
+    bool result = SFE_ADS122C04::begin(address(), wirePort);
+
+    if (!result)
+        Serial.println("ADS122C04 - begin failed");
+
+    result &= SFE_ADS122C04::configureADCmode(wireMode);
+    result &= SFE_ADS122C04::setDataRate(sampleRate);
+
+    return result;
+}
+
+//----------------------------------------------------------------------------------------------------------
+// onPropertyUpdate()
+//
+// Called when the value of a managed property was updated.
+//
+void spDevADS122C04::onPropertyUpdate(const char *propName)
+{
+    SFE_ADS122C04::configureADCmode(wireMode);
+    SFE_ADS122C04::setDataRate(sampleRate);
+}
+
---
+++ src/Spark/spDevADS122C04.h
@@ -0,0 +1,62 @@
+/*
+ *
+ *  spDevADS122C04.h
+ *
+ *  Spark Device object for the ADS122C04 A/D converter device.
+ * 
+ * 
+ */
+
+#pragma once
+
+#include "Arduino.h"
+#include "SparkFun_ADS122C04_ADC_Arduino_Library.h"
+#include "spDevice.h"
+
+
+
+#define kADS122C04DeviceName "ads122c04"
+
+// Define our class
+class spDevADS122C04 : public spDeviceType<spDevADS122C04>, public SFE_ADS122C04
+{
+
+  public:
+    spDevADS122C04();
+    // Interface
+    static bool isConnected(spDevI2C &i2cDriver, uint8_t address);
+
+    static const char *getDeviceName()
+    {
+        return kADS122C04DeviceName;
+    };
+
+    static const uint8_t *getDefaultAddresses()
+    {
+        return defaultDeviceAddress;
+    }
+    // holds the class list of possible addresses/IDs for this objects
+    static uint8_t defaultDeviceAddress[];
+
+    bool onInitialize(TwoWire &);
+
+    // Called when a managed property is updated
+    void onPropertyUpdate(const char *);
+
+private:
+    float read_temperature_c();
+    float read_temperature_f();
+    float read_internal_temperature();
+    float read_raw_voltage();
+
+public:
+    spPropertyUint8<spDevADS122C04> wireMode;
+    spPropertyUint8<spDevADS122C04> sampleRate;
+
+    // Define our output parameters - specify the get functions to call.
+    spParameterOutFloat<spDevADS122C04, &spDevADS122C04::read_temperature_c> temperatureC;
+    spParameterOutFloat<spDevADS122C04, &spDevADS122C04::read_temperature_f> temperatureF;    
+    spParameterOutFloat<spDevADS122C04, &spDevADS122C04::read_internal_temperature> internalTemperature;    
+    spParameterOutFloat<spDevADS122C04, &spDevADS122C04::read_raw_voltage> rawVoltage;    
+
+};
