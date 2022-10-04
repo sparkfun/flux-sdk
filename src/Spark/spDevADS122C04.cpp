4ccb48ca9348f34fe314d8bca2bf1a8f1caa83ea
Paul <pc235603@gmail.com>
2022-10-04 10:56:22 +0100

    spDevADS122C04: improved isConnected

--- src/Spark/spDevADS122C04.cpp
+++ src/Spark/spDevADS122C04.cpp
@@ -13,9 +13,7 @@
 
 
 // The ADS122C04 supports multiple addresses. The SparkX breakout can be set to 0x45, 0x44, 0x41, 0x40
-// 0x40 clashes with the humidity sensor on the MS8607 PHT sensor...
-// HACK: let's ignore 0x40 for now...
-uint8_t spDevADS122C04::defaultDeviceAddress[] = { 0x45, 0x44, 0x41, kSparkDeviceAddressNull};
+uint8_t spDevADS122C04::defaultDeviceAddress[] = { 0x45, 0x44, 0x41, 0x40, kSparkDeviceAddressNull};
 
 // Register this class with the system - this enables the *auto load* of this device
 spRegisterDevice(spDevADS122C04);
@@ -62,8 +60,37 @@ float spDevADS122C04::read_raw_voltage()
 
 bool spDevADS122C04::isConnected(spDevI2C &i2cDriver, uint8_t address)
 {
+    // 0x40 clashes with the humidity sensor on the MS8607 PHT sensor...
+    // We need to do something clever here to make sure we don't detect an MS8607 as an ADS122C04
 
-    return i2cDriver.ping(address);
+    // For speed, ping the device address first
+    if (!i2cDriver.ping(address))
+        return false;
+
+    // Read Config Register 2
+    uint8_t readCommand = ADS122C04_READ_CMD(ADS122C04_CONFIG_2_REG);
+    uint8_t configReg2 = 0;
+    if (!i2cDriver.readRegister(address, readCommand, &configReg2))
+        return false;
+    // Enable inverted data output
+    uint8_t writeCommand = ADS122C04_WRITE_CMD(ADS122C04_CONFIG_2_REG);
+    if (!i2cDriver.writeRegister(address, writeCommand, (configReg2 | (1 << 4)) & ~(1 << 5)))
+        return false;
+    delay(1);
+    // Now read configReg2 again. This time it should be the register contents followed by the inverted contents
+    uint16_t configReg2inv = 0;
+    if (!i2cDriver.readRegister16(address, readCommand, &configReg2inv))
+        return false;
+    uint8_t byte1 = (uint8_t)(configReg2inv & 0xFF);
+    uint8_t byte2 = configReg2inv >> 8;
+    if ( 0xFF != (byte1 ^ byte2)) // Check byte1 is the inverse of byte2 by ex-oring them together. Result should be 0xFF.
+        return false;
+    // Finally, restore Config Register 2
+    if (!i2cDriver.writeRegister(address, writeCommand, configReg2))
+        return false;
+    delay(1);
+
+    return true;
 }
 
 //----------------------------------------------------------------------------------------------------------
@@ -82,8 +109,7 @@ bool spDevADS122C04::onInitialize(TwoWire &wirePort)
     if (!result)
         spLog_E("ADS122C04 - begin failed");
 
-    result &= SFE_ADS122C04::configureADCmode(wireMode);
-    result &= SFE_ADS122C04::setDataRate(sampleRate);
+    result &= SFE_ADS122C04::configureADCmode(wireMode, sampleRate);
 
     return result;
 }
@@ -95,7 +121,6 @@ bool spDevADS122C04::onInitialize(TwoWire &wirePort)
 //
 void spDevADS122C04::onPropertyUpdate(const char *propName)
 {
-    SFE_ADS122C04::configureADCmode(wireMode);
-    SFE_ADS122C04::setDataRate(sampleRate);
+    SFE_ADS122C04::configureADCmode(wireMode, sampleRate);
 }
 
