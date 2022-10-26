ecaac72721fd52e8d6fd76dd78f0e9aa77e2cd38
Kirk Benell <kirk.benell@sparkfun.com>
2022-10-26 14:13:20 -0600

    moved out the editor interface -> new new interface include

--- src/Spark/spCoreTypes.h
+++ src/Spark/spCoreTypes.h
@@ -1098,29 +1098,5 @@ template <typename T> spTypeID spGetClassTypeID()
     
 };
 
-//----------------------------------------------------------------------
-// spDataEditor()
-//
-// define a data editor interface - called to have some value edited.
-//
-//
-// TODO: Factor to a different header in the future...
 
-class spDataEditor
-{
-
-  public:
-    virtual bool editField(char *value, size_t lenValue, uint32_t timeout = 60) = 0;
-    virtual bool editField(std::string &value, uint32_t timeout = 60) = 0;
-    virtual bool editField(bool &value, uint32_t timeout = 60) = 0;
-    virtual bool editField(int8_t &value, uint32_t timeout = 60) = 0;
-    virtual bool editField(int16_t &value, uint32_t timeout = 60) = 0;    
-    virtual bool editField(int32_t &value, uint32_t timeout = 60) = 0;
-    virtual bool editField(uint8_t &value, uint32_t timeout = 60) = 0;
-    virtual bool editField(uint16_t &value, uint32_t timeout = 60) = 0;    
-    virtual bool editField(uint32_t &value, uint32_t timeout = 60) = 0;
-    virtual bool editField(float &value, uint32_t timeout = 60) = 0;
-    virtual bool editField(double &value, uint32_t timeout = 60) = 0;
-    virtual void beep() = 0;
-};
 // End - spCoreTypes.h
