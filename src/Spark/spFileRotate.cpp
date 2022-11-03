15183d4273e45daaf5cc664b33742609523848be
Kirk Benell <kirk.benell@sparkfun.com>
2022-11-02 21:56:00 -0600

    makes use of the new FS interface defs; adds actual rotation

--- src/Spark/spFileRotate.cpp
+++ src/Spark/spFileRotate.cpp
@@ -1,6 +1,5 @@
 
 
-
 #include "spFileRotate.h"
 #include "spUtils.h"
 
@@ -9,70 +8,109 @@
 
 bool spFileRotate::getNextFilename(std::string &strFile)
 {
-	// FS Set?
-	if (!_theFS)
-		return false;
+    // FS Set?
+    if (!_theFS)
+        return false;
 
-	char szBuffer[64];
-	while(true)
-	{
-		_currentFileNumber++;
+    char szBuffer[64];
+    while (true)
+    {
+        _currentFileNumber++;
 
-		snprintf(szBuffer, sizeof(szBuffer), "/%s%04d.txt", filePrefix.get().c_str(), _currentFileNumber);
-		
-		// - does this file already exist?
-		if ( !_theFS->exists(szBuffer)) 
-			break; // free
+        snprintf(szBuffer, sizeof(szBuffer), "%s%04d.txt", filePrefix.get().c_str(), _currentFileNumber);
 
-	}
-	strFile = szBuffer;
+        // - does this file already exist?
+        if (!_theFS->exists(szBuffer))
+            break; // free
+    }
+    strFile = szBuffer;
 
-	return true;
+    // TODO - save the file name/number to NVR
+    return true;
 }
+
+//------------------------------------------------------------------------------------------------
+// Open the next log file.
+
+bool spFileRotate::openNextLogFile(bool bSendEvent)
+{
+
+    if (_currentFile)
+    {
+        _currentFile.close();
+        _currentFile = spFSFile();
+    }
+    // Open the next file
+    std::string nextFile;
+    if (!getNextFilename(nextFile))
+        return false;
+
+    _currentFilename = nextFile;
+
+    _currentFile = _theFS->open(_currentFilename.c_str(), spIFileSystem::kFileWrite, true);
+
+    if (!_currentFile)
+    {
+        spLog_E(F("File Rotate - Unable to create file %s"), _currentFilename.c_str());
+        return false;
+    }
+    _ticksFileOpen = millis();
+
+    _flushCount = 0; // new file, new start
+
+    // send the new file event.
+    if (bSendEvent)
+        on_newFile.emit();
+
+    return true;
+}
+
+//------------------------------------------------------------------------------------------------
 void spFileRotate::write(int value)
 {
-	write( sp_utils::to_string(value).c_str());
+    write(sp_utils::to_string(value).c_str());
 }
 
+//------------------------------------------------------------------------------------------------
 void spFileRotate::write(float value)
 {
-	write( sp_utils::to_string(value).c_str());
+    write(sp_utils::to_string(value).c_str());
 }
 
-void spFileRotate::write(const char * value)
+//------------------------------------------------------------------------------------------------
+void spFileRotate::write(const char *value)
 {
 
-	if(!_theFS)
-	{
-		spLog_E(F("File Rotate - Unable to output to file. No filesystem set"));
-		return;
-	}
+    if (!_theFS)
+    {
+        spLog_E(F("File Rotate - Unable to output to file. No filesystem set"));
+        return;
+    }
 
-	if (!_currentFile)
-	{
-		// Open the next file
-		std::string nextFile; 
-		if (!getNextFilename(nextFile))
-			return;
+    // no file - system just starting up?
+    if (!_currentFile)
+    {
+        // open the next file - dont' send an event - th
+        if (!openNextLogFile(false))
+            return;
+    }
+    // Write the current line out
+    _currentFile.write((uint8_t *)value, strlen(value) + 1);
+    // add a cr
+    _currentFile.write((uint8_t *)"\n", 1);
 
-		_currentFilename = nextFile;
+    // Will we need to rotate?
+    if (millis() - _ticksFileOpen > _ticksRotPeriod)
+    {
+        // open the next file, send the new file event. This will cause
+        // the next line out to be a "start of the file line" (i.e. header)
+        // if that's how the format rolls
+        if (!openNextLogFile(true))
+            return;
+    }
 
-		_currentFile = _theFS->open(_currentFilename.c_str(), spIFileSystem::kFileWrite, true);
-
-		if ( !_currentFile)
-		{
-			spLog_E(F("File Rotate - Unable to create file %s"), _currentFilename.c_str());
-			return;
-		}
-		_ticksFileOpen = millis();
-	}
-
-	_currentFile.write((uint8_t*)value, strlen(value)+1);
-	// add a cr
-	_currentFile.write((uint8_t*)"\n", 1);
-
-	_flushCount = (_flushCount+1)%kFlushIncrement;
-	if( !_flushCount)
-		_currentFile.flush();
-
+    // flush the file buffer?
+    _flushCount = (_flushCount + 1) % kFlushIncrement;
+    if (!_flushCount)
+        _currentFile.flush();
 }
--- src/Spark/spFileRotate.h
+++ src/Spark/spFileRotate.h
@@ -2,10 +2,12 @@
 
 #pragma once
 
-#include "spCoreInterface.h"
 #include "spCore.h"
-#include "spFSSDCard.h"
+#include "spCoreInterface.h"
+#include "spFS.h"
 
+// TODO - refactor this out
+#include "spFSSDMMCard.h"
 #include <string>
 
 // This object implements the spWriter interface, and manages the rotation
@@ -14,77 +16,68 @@
 class spFileRotate : public spActionType<spFileRotate>, public spWriter
 {
 
-private:
-
-	// rotation period setter/getter
+  private:
+    // rotation period setter/getter
     uint get_RotatePeriod(void)
     {
-        return _ticksRotPeriod/kTicksPerHour;
+        return _ticksRotPeriod / kTicksPerHour;
     }
-    void set_RotatePeriod( uint hours)
+    void set_RotatePeriod(uint hours)
     {
         _ticksRotPeriod = hours * kTicksPerHour;
     }
 
-public:
-	spFileRotate(): _currentFilename{""}, _theFS{nullptr}, _flushCount{0}, 
-					_ticksFileOpen{0}, _ticksRotPeriod{0} {
+  public:
+    spFileRotate() : _currentFilename{""}, _theFS{nullptr}, _flushCount{0}, _ticksFileOpen{0}, _ticksRotPeriod{0}
+    {
 
-		setName("File Rotate", 
-				"Writes output to a file on the provided filesystem. Rotates files after a given time period.");
+        setName("File Rotate",
+                "Writes output to a file on the provided filesystem. Rotates files after a given time period.");
 
-		spRegister(rotatePeriod, "Rotate Period", "Time between file rotation.");
-		spRegister(startNumber, "File Start Number", "The Number the filename rotation starts with");
-		spRegister(filePrefix, "Filename Prefix", "The prefix string for the generated filenames");
+        spRegister(rotatePeriod, "Rotate Period", "Time between file rotation.");
+        spRegister(startNumber, "File Start Number", "The Number the filename rotation starts with");
+        spRegister(filePrefix, "Filename Prefix", "The prefix string for the generated filenames");
 
-		// at startup, current file count == startNumber-1
-		_currentFileNumber = startNumber.get()-1;
+        // at startup, current file count == startNumber-1
+        _currentFileNumber = startNumber.get() - 1;
+    };
 
-
-	};
-
-	void write(int );
+    void write(int);
     void write(float);
     void write(const char *);
 
     void setFileSystem(spIFileSystem *fs)
     {
-    	if (fs)
-    		_theFS = fs;
+        if (fs)
+            _theFS = fs;
     }
     void setFileSystem(spIFileSystem &fs)
     {
-    	setFileSystem(&fs);
+        setFileSystem(&fs);
     }
     // Rotation Period in Days
-    spPropertyRWUint<spFileRotate, &spFileRotate::get_RotatePeriod, &spFileRotate::set_RotatePeriod>  rotatePeriod={24, {
-                                            									{"6 Hours", 6},
-                                            									{"12 Hours", 12},
-                                            									{"1 Day", 24},        
-                                            									{"2 Days", 48},      
-                                            									{"1 Week", 168}                       
-                                            									}};
+    spPropertyRWUint<spFileRotate, &spFileRotate::get_RotatePeriod, &spFileRotate::set_RotatePeriod> rotatePeriod = {
+        24, {{"6 Hours", 6}, {"12 Hours", 12}, {"1 Day", 24}, {"2 Days", 48}, {"1 Week", 168}}};
 
-    spPropertyUint<spFileRotate>   startNumber = {1};
+    spPropertyUint<spFileRotate> startNumber = {1};
 
     spPropertyString<spFileRotate> filePrefix = {""};
 
-private:
+    // Our "New File" event
+    spSignalVoid on_newFile;
 
-	static constexpr uint kTicksPerHour = 3600000;
+  private:
+    static constexpr uint kTicksPerHour = 3600000;
 
-	bool getNextFilename(std::string &strFile);
+    bool getNextFilename(std::string &strFile);
+    bool openNextLogFile(bool bSendEvent = true);
 
-	std::string 	_currentFilename;
-	spIFileSystem * _theFS;
-	uint8_t         _flushCount;
-	uint32_t        _ticksFileOpen;
-	uint32_t        _ticksRotPeriod;
-	uint            _currentFileNumber;
+    std::string _currentFilename;
+    spIFileSystem *_theFS;
+    uint8_t _flushCount;
+    uint32_t _ticksFileOpen;
+    uint32_t _ticksRotPeriod;
+    uint _currentFileNumber;
 
-	spFSFile   		_currentFile; 
-
-
-
-
+    spFSFile _currentFile;
 };
