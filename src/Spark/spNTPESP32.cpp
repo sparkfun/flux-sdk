82dcb1170ee99c3d726135257c169846336ed21e
Kirk D. Benell <kirk.benell@sparkfun.com>
2022-11-05 13:14:10 -0600

    initial impl of a ntp client

---
+++ src/Spark/spNTPESP32.cpp
@@ -0,0 +1,72 @@
+
+
+
+
+#ifdef ESP32
+
+#include "spNTPESP32.h"
+
+#include "time.h"
+#include "lwip/apps/sntp.h"
+
+#define kNTPServerAddress "pool.ntp.org"
+//----------------------------------------------------------------
+// Enabled Property setter/getters
+void spNTPESP32::set_isEnabled(bool bEnabled)
+{
+    // Any changes?
+    if (_isEnabled == bEnabled)
+        return;  
+
+    // changing state ...
+    // TODO - Should this control connection state?
+
+    _isEnabled = bEnabled;
+
+    if ( _isEnabled)
+        (void)start();
+    else 
+        stop();
+}
+
+//----------------------------------------------------------------
+bool spNTPESP32::get_isEnabled(void)
+{
+    return _isEnabled;
+}
+
+// Event callback 
+//----------------------------------------------------------------------------
+void spNTPESP32::onConnectionChange(bool bConnected)
+{
+    // Are we enabled ...
+    if (!_isEnabled)
+        return; 
+
+    if (bConnected)
+        start();
+    else
+        stop();
+}
+
+bool spNTPESP32::start(void)
+{
+    // already running?
+    if(sntp_enabled())
+        return true;
+
+    // Leverage ESP32 Arduino setup
+    configTime(gmtOffsetMinutes()*60, daylightOffsetMinutes()*60, kNTPServerAddress);
+
+    return sntp_enabled();
+}
+
+void spNTPESP32::stop(void)
+{
+    // If the sntp system is up, shut it down
+    if(sntp_enabled())
+        sntp_stop();
+}
+    
+
+#endif
---
+++ src/Spark/spNTPESP32.h
@@ -0,0 +1,59 @@
+
+
+#pragma once
+
+#ifdef ESP32
+
+#include "spSpark.h"
+
+
+// Use the NTP client on the ESP32 to sync the onboard clock
+
+class spNTPESP32 : public spActionType<spNTPESP32>
+{
+private:
+    void set_isEnabled(bool bEnabled);
+    bool get_isEnabled(void);
+
+    // Event callback 
+    //----------------------------------------------------------------------------
+    void onConnectionChange(bool bConnected);
+
+public:
+    spNTPESP32() : _isEnabled{true}
+    {
+        spRegister(enabled, "Enabled", "Enable or Disable the NTP Client");
+
+        spRegister(gmtOffsetMinutes, "GMT Offset", "GMT offset in minutes for timezone settings");
+        spRegister(daylightOffsetMinutes, "Daylight Offset", "Daylight Savings offset in minutes for timezone settings");        
+
+        setName("NTP Client", "NTP Time Synch Client");
+
+        spark.add(this);
+    };
+
+    bool start(void);
+    void stop(void);
+
+    // Used to register the event we want to listen to, which will trigger this
+    // activity.
+    void listenToConnection(spSignalBool &theEvent)
+    {
+        // Regisgter to get notified on connection changes
+        theEvent.call(this, &spNTPESP32::onConnectionChange);
+    }
+
+    // Properties 
+
+    // Enabled/Disabled
+    spPropertyRWBool<spNTPESP32, &spNTPESP32::get_isEnabled, &spNTPESP32::set_isEnabled> enabled;
+
+    // Time offsets
+    spPropertyUint<spNTPESP32>       gmtOffsetMinutes = {0};
+    spPropertyUint<spNTPESP32>       daylightOffsetMinutes = {0};
+
+private:
+	bool  _isEnabled;
+};
+
+#endif
