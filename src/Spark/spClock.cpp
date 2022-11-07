ecf9e55b566060eaf266722018c0a0fbad4133ae
Kirk Benell <kirk.benell@sparkfun.com>
2022-11-06 19:19:41 -0700

    class to manage clocks/time and time refs in the system - very simple impl

---
+++ src/Spark/spClock.cpp
@@ -0,0 +1,96 @@
+
+
+#include "spClock.h"
+
+#include "spSpark.h"
+
+// A little hacky - maybe ...
+#ifdef ESP32
+
+#define DEFAULT_DEFINED
+spClockESP32 defaultClock;
+
+#endif
+
+// Global object - for quick access to Settings system
+_spClock &spClock = _spClock::get();
+
+// Refresh period if we have a reference clock - time in secs
+
+#define kClockReferenceRefresh 3600
+
+_spClock::_spClock() : _defaultClock{nullptr}, _referenceClock{nullptr}, _lastRefCheck{0}, _bInitialized{false}
+{
+    // Set name and description
+    setName("Epoch Clock", "Seconds since Unix Epoch");
+
+    spark.add(this);
+
+#ifdef DEFAULT_DEFINED
+    setDefaultClock(&defaultClock);
+#endif
+}
+//----------------------------------------------------------------
+uint32_t _spClock::epoch()
+{
+    if (_defaultClock)
+        return _defaultClock->epoch();
+
+    // We need something ...
+
+    return millis() / 1000; // TODO - Revisit
+}
+//----------------------------------------------------------------
+uint32_t _spClock::now()
+{
+    return epoch();
+}
+//----------------------------------------------------------------
+void _spClock::setDefaultClock(spIClock *clock)
+{
+    if (clock)
+        _defaultClock = clock;
+}
+//----------------------------------------------------------------
+void _spClock::setClockReference(spIClock *clock)
+{
+
+    if (!clock)
+        return;
+
+    _referenceClock = clock;
+
+    updateClock();
+}
+//----------------------------------------------------------------
+void _spClock::updateClock()
+{
+
+    // refersh our clock
+    if (_bInitialized && _defaultClock && _referenceClock)
+    {
+        uint32_t epoch = _referenceClock->epoch();
+        if (epoch)
+            _defaultClock->set(epoch);
+    }
+
+    _lastRefCheck = epoch();
+}
+//----------------------------------------------------------------
+bool _spClock::initialize(void)
+{
+    _bInitialized = true;
+    updateClock();
+    return true;
+}
+
+bool _spClock::loop(void)
+{
+    // Time to refresh?
+    if (_bInitialized && epoch() - _lastRefCheck > kClockReferenceRefresh)
+    {
+        updateClock();
+        return true;
+    }
+    return false;
+}
---
+++ src/Spark/spClock.h
@@ -0,0 +1,79 @@
+
+
+// Action to control save of settings ...
+#pragma once
+
+#include "spCore.h"
+
+// Define a clock interface -- really just want secs from unix epoch
+class spIClock
+{
+  public:
+    virtual uint32_t epoch(void) = 0;
+    virtual void set(uint32_t) = 0;
+};
+
+#ifdef ESP32
+
+#include <time.h>
+class spClockESP32 : public spIClock
+{
+  public:
+    uint32_t epoch(void)
+    {
+        time_t now;
+        time(&now);
+        return now;
+    }
+
+    void set(uint32_t refEpoch)
+    {
+        timeval epoch = {(time_t)refEpoch, 0};
+        const timeval *tv = &epoch;
+        timezone utc = {0, 0};
+        const timezone *tz = &utc;
+        settimeofday(tv, tz);
+    }
+};
+#endif
+
+class _spClock : public spActionType<_spClock>
+{
+
+  public:
+    // spClock is a singleton
+    static _spClock &get(void)
+    {
+
+        static _spClock instance;
+        return instance;
+    }
+    // This is a singleton class - so delete copy & assignment constructors
+    _spClock(_spClock const &) = delete;
+    void operator=(_spClock const &) = delete;
+
+    uint32_t epoch();
+
+    uint32_t now();
+
+    void setDefaultClock(spIClock *clock);
+
+    void setClockReference(spIClock *clock);
+
+    bool loop(void);
+
+    bool initialize(void);
+
+  private:
+    _spClock();
+
+    void updateClock();
+
+    spIClock *_defaultClock;
+    spIClock *_referenceClock;
+
+    uint32_t _lastRefCheck;
+
+    bool _bInitialized;
+};
+extern _spClock &spClock;
