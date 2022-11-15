1273063404775e20e25adca7310dc91c999464ac
Kirk D. Benell <kirk.benell@sparkfun.com>
2022-11-15 13:31:14 -0700

    initial entry of JSON settings storage; compiles; no tested

---
+++ src/Spark/spStorageJSONPref.cpp
@@ -0,0 +1,340 @@
+/*
+#
+ */
+
+#ifdef JSON
+
+#include "spStorageJSONPref.h"
+#include "spCoreLog.h"
+#include "spUtils.h"
+
+
+#define kJsonDocumentSize 2048
+//------------------------------------------------------------------------------
+// Use tags to ID an item and move to use data types. Model after the
+// JSON preference library
+
+// Write out a bool value
+bool spStorageJSONBlock::writeBool(const char *tag, bool value)
+{
+    if (!_jSection.isNull())
+    {
+        (_jSection)[tag] = value;
+        return true;
+    }
+    return false;
+}
+//------------------------------------------------------------------------
+// write out an int8 value
+
+bool spStorageJSONBlock::writeInt8(const char *tag, int8_t value)
+{
+    if (!_jSection.isNull())
+    {
+        (_jSection)[tag] = value;
+        return true;
+    }
+    return false;
+}
+
+//------------------------------------------------------------------------
+// write out an int16 value
+
+bool spStorageJSONBlock::writeInt16(const char *tag, int16_t value)
+{
+    if (!_jSection.isNull())
+    {
+        (_jSection)[tag] = value;
+        return true;
+    }
+    return false;
+}
+
+//------------------------------------------------------------------------
+// write out a int value
+
+bool spStorageJSONBlock::writeInt32(const char *tag, int32_t value)
+{
+    if (!_jSection.isNull())
+    {
+        (_jSection)[tag] = value;
+        return true;
+    }
+    return false;
+}
+//------------------------------------------------------------------------
+// Unsigned int8  - aka uchar
+
+bool spStorageJSONBlock::writeUInt8(const char *tag, uint8_t value)
+{
+    if (!_jSection.isNull())
+    {
+        (_jSection)[tag] = value;
+        return true;
+    }
+    return false;
+}
+
+//------------------------------------------------------------------------
+// Unsigned int16  - aka ushort
+
+bool spStorageJSONBlock::writeUInt16(const char *tag, uint16_t value)
+{
+    if (!_jSection.isNull())
+    {
+        (_jSection)[tag] = value;
+        return true;
+    }
+    return false;
+}
+
+//------------------------------------------------------------------------
+bool spStorageJSONBlock::writeUInt32(const char *tag, uint32_t value)
+{
+    if (!_jSection.isNull())
+    {
+        (_jSection)[tag] = value;
+        return true;
+    }
+    return false;
+}
+//------------------------------------------------------------------------
+// write out a float
+
+bool spStorageJSONBlock::writeFloat(const char *tag, float value)
+{
+    if (!_jSection.isNull())
+    {
+        (_jSection)[tag] = value;
+        return true;
+    }
+    return false;
+}
+//------------------------------------------------------------------------
+// double
+
+bool spStorageJSONBlock::writeDouble(const char *tag, double value)
+{
+    if (!_jSection.isNull())
+    {
+        (_jSection)[tag] = value;
+        return true;
+    }
+    return false;
+}
+//------------------------------------------------------------------------
+// Write out a c string
+bool spStorageJSONBlock::writeString(const char *tag, const char *value)
+{
+
+    if (!_jSection.isNull())
+    {
+        (_jSection)[tag] = value;
+        return true;
+    }
+    return false;
+
+}
+//------------------------------------------------------------------------
+// Read value section
+//------------------------------------------------------------------------
+
+bool spStorageJSONBlock::readBool(const char *tag, bool &value, bool defaultValue)
+{
+    if (!_jSection.isNull())
+        vlaue = (_jSection)[tag];
+    else
+        value = defaultValue;
+
+    return true;
+}
+//------------------------------------------------------------------------
+bool spStorageJSONBlock::readInt8(const char *tag, int8_t &value, int8_t defaultValue)
+{
+    if (!_jSection.isNull())
+        vlaue = (_jSection)[tag];
+    else
+        value = defaultValue;
+
+    return true;
+}
+
+
+//------------------------------------------------------------------------
+bool spStorageJSONBlock::readInt16(const char *tag, int16_t &value, int16_t defaultValue)
+{
+    if (!_jSection.isNull())
+        vlaue = (_jSection)[tag];
+    else
+        value = defaultValue;
+
+    return true;
+}
+
+//------------------------------------------------------------------------
+bool spStorageJSONBlock::readInt32(const char *tag, int32_t &value, int32_t defaultValue)
+{
+    if (!_jSection.isNull())
+        vlaue = (_jSection)[tag];
+    else
+        value = defaultValue;
+
+    return true;
+}
+
+//------------------------------------------------------------------------
+bool spStorageJSONBlock::readUInt8(const char *tag, uint8_t &value, uint8_t defaultValue)
+{
+    if (!_jSection.isNull())
+        vlaue = (_jSection)[tag];
+    else
+        value = defaultValue;
+
+    return true;
+}
+
+//------------------------------------------------------------------------
+bool spStorageJSONBlock::readUInt16(const char *tag, uint16_t &value, uint16_t defaultValue)
+{
+    if (!_jSection.isNull())
+        vlaue = (_jSection)[tag];
+    else
+        value = defaultValue;
+
+    return true;
+}
+
+//------------------------------------------------------------------------
+bool spStorageJSONBlock::readUInt32(const char *tag, uint32_t &value, uint32_t defaultValue)
+{
+    if (!_jSection.isNull())
+        vlaue = (_jSection)[tag];
+    else
+        value = defaultValue;
+
+    return true;
+}
+
+//------------------------------------------------------------------------
+bool spStorageJSONBlock::readFloat(const char *tag, float &value, float defaultValue)
+{
+    if (!_jSection.isNull())
+        vlaue = (_jSection)[tag];
+    else
+        value = defaultValue;
+
+    return true;
+}
+
+//------------------------------------------------------------------------
+bool spStorageJSONBlock::readDouble(const char *tag, double &value, double defaultValue)
+{
+    if (!_jSection.isNull())
+        vlaue = (_jSection)[tag];
+    else
+        value = defaultValue;
+
+    return true;
+}
+
+//------------------------------------------------------------------------
+size_t spStorageJSONBlock::readString(const char *tag, char *data, size_t len)
+{
+
+
+    if (_jSection.isNull())
+        return 0
+
+    std::string value = (_jSection)[tag];
+
+    return strlcpy(data, value.c_str(), len);
+
+
+}
+
+//------------------------------------------------------------------------------
+bool spStorageJSONBlock::valueExists(const char *tag)
+{
+     if (!_jSection.isNull())
+        return _jSection.containsKey(tag);
+     else
+        return false;
+
+}
+//------------------------------------------------------------------------------
+// spStorage
+//
+// Interface for a storage system to persist state of a system
+
+bool spStorageJSONPref::begin(void)
+{
+
+    _pDocument = new DynamicJsonDocument(kJsonDocumentSize);
+
+    if (!_pDocument)
+    {
+        spLog_E(F("Unable to create JSON object for preferences."));
+        return false;
+    }
+
+}
+
+void spStorageJSONPref::end(void)
+{
+    // TODO - write out the data
+    std::string value;
+
+    if (!serializeJsonPretty(*_pDocument, value))
+        spLog_E(F("Unable to generate settings JSON string"));
+    else
+    {
+        // TODO - output to file...
+        Serial.println(value.c_str());
+    }
+    // Clear memory
+    if (_pDocument)
+    {
+        delete _pDocument;
+        _pDocument = nullptr;
+    }
+}
+
+// public methods to manage a block
+spStorageJSONBlock *spStorageJSONPref::beginBlock(const char *tag)
+{
+
+    if (!tag)
+        return nullptr;
+
+    JsonObject jObj;
+
+    // Does the object already exists?
+    _jObj = _pDocument->[tag];
+    if (jObj.isNull())
+        jObj = _pDocument->createNestedObject(tag)
+
+    _theBlock.setObject(jObj);
+
+    return &_theBlock;
+}
+
+spStorageJSONBlock *spStorageJSONPref::getBlock(const char *tag)
+{
+
+    // TODO - find object in JSON doc, set in block and return that
+
+    return beginBlock(tag);
+}
+void spStorageJSONPref::endBlock(spStorageBlock *)
+{
+    _prefs.end();
+}
+
+void spStorageJSONPref::resetStorage()
+{
+    // call low level ESP IDF functions to do this - as recommended by JSON docs ...
+    nvs_flash_erase();
+    nvs_flash_init();
+}
+
+#endif
---
+++ src/Spark/spStorageJSONPref.h
@@ -0,0 +1,90 @@
+/*
+ * spStorage.h
+ *
+ * Define our interfaces for state saving/storage
+ */
+
+#pragma once
+
+#include "spStorage.h"
+
+
+#include <Arduino.h>
+#include <ArduinoJson.h>
+//------------------------------------------------------------------------------
+// Store prefs to a JSON file 
+
+class spStorageJSONPref;
+
+class spStorageJSONBlock : public spStorageBlock {
+
+public:
+    spStorageJSONBlock(){}
+
+    bool writeBool(const char* tag, bool data);
+    bool writeInt8(const char* tag, int8_t data);
+    bool writeInt16(const char* tag, int16_t data);
+    bool writeInt32(const char* tag, int32_t data);
+    bool writeUInt8(const char* tag, uint8_t data);
+    bool writeUInt16(const char* tag, uint16_t data);
+    bool writeUInt32(const char* tag, uint32_t data);
+    bool writeFloat(const char* tag, float data);
+    bool writeDouble(const char* tag, double data);
+    bool writeString(const char* tag, const char* data);
+
+    bool readBool(const char* tag, bool& value, bool defaultValue = false);
+    bool readInt8(const char* tag, int8_t& value, int8_t defaultValue = 0);
+    bool readInt16(const char* tag, int16_t& value, int16_t defaultValue = 0);
+    bool readInt32(const char* tag, int32_t& value, int32_t defaultValue = 0);
+    bool readUInt8(const char* tag, uint8_t& value, uint8_t defaultValue = 0);
+    bool readUInt16(const char* tag, uint16_t& value, uint16_t defaultValue = 0);
+    bool readUInt32(const char* tag, uint32_t& value, uint32_t defaultValue = 0);
+    bool readFloat(const char* tag, float& value, float defaultValue = NAN);
+    bool readDouble(const char* tag, double& value, double defaultValue = NAN);
+    size_t readString(const char* tag, char* data, size_t len);
+
+    bool valueExists(const char* tag);
+
+private:
+    friend spStorageJSONPref;
+
+    JsonObject _jsonSection;
+
+    void setObject(JsonObject &jsonSection)
+    {
+        _jsonSection = JsonObject;
+    }
+};
+
+//------------------------------------------------------------------------------
+// spStorage
+//
+// Interface for a storage system to persist state of a system
+
+class spStorageJSONPref : public spStorage {
+
+public:
+    spStorageJSONPref() : _pDocument{nullptr}{}
+
+    // add begin, end stubs - the Esp32 prefs system doesn't required trasaction brackets
+    bool begin(void);
+
+    void end(void);
+    
+    // public methods to manage a block
+    spStorageJSONBlock* beginBlock(const char* tag);
+
+    spStorageJSONBlock* getBlock(const char* tag);
+    void endBlock(spStorageBlock*);
+
+    void resetStorage();
+
+private:
+    // The block used to interface with the system
+    spStorageJSONBlock _theBlock;
+
+    // Pointer to the json documement 
+
+    JsonDocument  * _pDocument;
+};
+
