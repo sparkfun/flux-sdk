fe63f15184dff985f6a0bffd65e69d80ff52832d
Paul <pc235603@gmail.com>
2022-11-29 13:51:50 +0000

    Enhance Bio Hub - add initialize

--- src/Spark/spDevBioHub.cpp
+++ src/Spark/spDevBioHub.cpp
@@ -36,9 +36,6 @@ spDevBioHub::spDevBioHub()
     setName(getDeviceName());
     setDescription("MAX30101 Biometric Hub");
 
-    _begun = false;
-    _initialized = false;
-
     // Register parameters
     spRegister(heartRate, "Heart Rate (BPM)", "Heart Rate (Beats Per Minute)");
     spRegister(confidence, "Confidence Level (%)", "Confidence Level (%)");
@@ -46,43 +43,59 @@ spDevBioHub::spDevBioHub()
     spRegister(status, "Status", "Status");
     spRegister(eStatus, "Extended Status", "Extended Status");
     spRegister(O2R, "Oxygen R Value", "Oxygen R Value");
-
-    // Register read-write properties
-    spRegister(resetPin, "Reset Pin GPIO #", "Reset Pin GPIO #");
-    spRegister(mfioPin, "MFIO Pin GPIO #", "MFIO Pin GPIO #");
 }
 
-//----------------------------------------------------------------------------------------------------------
-// Static method used to determine if devices is connected before creating this object (if creating dynamically)
-bool spDevBioHub::isConnected(spBusI2C &i2cDriver, uint8_t address, int connectResetPin, int connectMfioPin )
+// The Bio Hub is a special case. We need to know the reset and mfio pins in order to
+// check if it is connected and to initialize it. During auto-detect the pins aren't known,
+// so auto-detect will fail. We need to manually create an instance of the spDevBioHub,
+// initialize the pin numbers with `initialize`, check if it is connected, call onInitialize
+// and then we can add it to the logger.
+bool spDevBioHub::initialize( int connectResetPin, int connectMfioPin )
 {
-    // The MAX32664 does not respond on the I2C bus until it has been reset correctly via its reset pin.
-    // So we cannot ping it, or read its registers here - unless we know the pins.
-
-    if ((connectResetPin < 0) || (connectMfioPin < 0))
+    _resetPin = connectResetPin;
+    _mfioPin = connectMfioPin;
+    
+    if ((_resetPin < 0) || (_mfioPin < 0))
         return false;
 
     // Reset the max32664
-    pinMode(connectMfioPin, OUTPUT);
-    digitalWrite(connectMfioPin, HIGH);
-    pinMode(connectResetPin, OUTPUT);
-    digitalWrite(connectResetPin, LOW);
+    pinMode(_mfioPin, OUTPUT);
+    digitalWrite(_mfioPin, HIGH);
+    pinMode(_resetPin, OUTPUT);
+    digitalWrite(_resetPin, LOW);
     delay(10);
-    digitalWrite(connectResetPin, HIGH);
+    digitalWrite(_resetPin, HIGH);
     delay(1000);
-    pinMode(connectMfioPin, INPUT_PULLUP); // To be used as an interrupt later
+    pinMode(_mfioPin, INPUT_PULLUP); // To be used as an interrupt later
 
     uint8_t identity[2] = { 0xFF, 0x00 };
-    bool couldBeBio = i2cDriver.write(address, identity, 2);
+    bool couldBeBio = spark.i2cDriver().write(getDefaultAddress(), identity, 2);
     if (couldBeBio)
     {
         uint8_t mcuType[2];
-        couldBeBio &= i2cDriver.receiveResponse(address, mcuType, 2);
+        couldBeBio &= spark.i2cDriver().receiveResponse(getDefaultAddress(), mcuType, 2) == 2;
         couldBeBio &= (mcuType[0] == 0x00); // 0x00: SUCCESS
         couldBeBio &= (mcuType[1] == 0x01); // 0x00: MAX32625; 0x01: MAX32660/MAX32664
     }
 
-    return couldBeBio;
+    if (couldBeBio)
+    {
+        TwoWire *wirePort = spark.i2cDriver().getWirePort();
+        if (onInitialize(*wirePort))
+            return true;
+    }
+    
+    return false;
+}
+
+//----------------------------------------------------------------------------------------------------------
+// Static method used to determine if devices is connected before creating this object (if creating dynamically)
+bool spDevBioHub::isConnected(spBusI2C &i2cDriver, uint8_t address)
+{
+    // The MAX32664 does not respond on the I2C bus until it has been reset correctly via its reset pin.
+    // So we cannot ping it, or read its registers here - unless we know the pins.
+
+    return false;
 }
 
 //----------------------------------------------------------------------------------------------------------
@@ -92,54 +105,13 @@ bool spDevBioHub::isConnected(spBusI2C &i2cDriver, uint8_t address, int connectR
 //
 // Place to initialized the underlying device library/driver
 //
-bool spDevBioHub::onInitialize(TwoWire &wirePort, int connectResetPin, int connectMfioPin )
+bool spDevBioHub::onInitialize(TwoWire &wirePort)
 {
-    _initialized = true;
-
-    _bioI2cPort = &wirePort; // Cheat... Keep a local record of which wirePort is being used, so we can call begin again
-
-    // For now, I think we need to accept that the Bio Hub cannot be detected / begun successfully unless the reset
-    // and MFIO pins are defined.
-    // Right now - I can't think of a better way to do this.
-
-    if (connectResetPin >= 0)
-        _resetPin = connectResetPin;
-    if (connectMfioPin >= 0)
-        _mfioPin = connectMfioPin;
-
     // On success, begin will return 0x00. It will return 0xFF if the pins have not been defined
     _begun = SparkFun_Bio_Sensor_Hub::begin(wirePort, _resetPin, _mfioPin) == 0x00;
     if (_begun)
         SparkFun_Bio_Sensor_Hub::configBpm(MODE_TWO); // MODE_TWO provides the oxygen R value
     return _begun;
-}
-
-// get/set methods for our read-write properties
-int spDevBioHub::get_reset_pin() { return _resetPin; }
-void spDevBioHub::set_reset_pin(int pin)
-{
-    _resetPin = pin;
-
-    if (_initialized)
-    {
-        // On success, begin will return 0x00. It will return 0xFF if either pin is undefined
-        _begun = SparkFun_Bio_Sensor_Hub::begin(*_bioI2cPort, _resetPin, _mfioPin) == 0x00;
-        if (_begun)
-            SparkFun_Bio_Sensor_Hub::configBpm(MODE_TWO); // MODE_TWO provides the oxygen R value
-    }
-}
-int spDevBioHub::get_mfio_pin() { return _mfioPin; }
-void spDevBioHub::set_mfio_pin(int pin)
-{
-    _mfioPin = pin;
-
-    if (_initialized)
-    {
-        // On success, begin will return 0x00. It will return 0xFF if either pin is undefined
-        _begun = SparkFun_Bio_Sensor_Hub::begin(*_bioI2cPort, _resetPin, _mfioPin) == 0x00;
-        if (_begun)
-            SparkFun_Bio_Sensor_Hub::configBpm(MODE_TWO); // MODE_TWO provides the oxygen R value
-    }
 }
 
 // GETTER methods for output params
