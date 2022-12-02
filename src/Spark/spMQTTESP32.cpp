27d35966445f3b7313f01efb27d5390e04cd55aa
Kirk Benell <kirk.benell@sparkfun.com>
2022-12-02 13:13:45 -0700

    refactor the core to a template; Enables standard and secure MQTT connections

--- src/Spark/spMQTTESP32.cpp
+++ src/Spark/spMQTTESP32.cpp
@@ -4,197 +4,28 @@
 
 #include "spMQTTESP32.h"
 
-#define kMaxConnectionTries 3
-#define kConnectionDelayMS 700
-
-//----------------------------------------------------------------
-// Enabled Property setter/getters
-void spMQTTESP32::set_isEnabled(bool bEnabled)
-{
-    // Any changes?
-    if (_isEnabled == bEnabled)
-        return;
-
-    _isEnabled = bEnabled;
-
-    // TODO chech network connection availablity ...
-    if (_isEnabled)
-        (void)connect();
-    else
-        disconnect();
-}
-
-//----------------------------------------------------------------
-bool spMQTTESP32::get_isEnabled(void)
-{
-    return _isEnabled;
-}
-
-// Event callback
 //----------------------------------------------------------------------------
-void spMQTTESP32::onConnectionChange(bool bConnected)
-{
-    // Are we enabled ...
-    if (!_isEnabled)
-        return;
-
-    // Anything change?
-    if (bConnected == connected())
-        return; 
-
-    if (bConnected)
-    {
-        spLog_I_(F("Connecting to MQTT endpoint %s:%u ..."), server().c_str(), port());
-        if (connect())
-            spLog_N(F("Connected"));
-        // the connect method will print out sufficient error messages 
-    }
-    else
-    {
-        spLog_I(F("Disconnecting from MQTT endpoint %s"), clientName().c_str());
-        disconnect();
-    }
-}
-//----------------------------------------------------------------------------
-
-bool spMQTTESP32::connected()
-{
-//    return (_isEnabled && _wifiClient.connected() != 0 && _mqttClient && _mqttClient->connected() != 0);
-    return (_isEnabled && _wifiClient.connected() != 0 && _mqttClient.connected() != 0);    
-}
-
-//----------------------------------------------------------------------------
-void spMQTTESP32::disconnect(void)
-{
-    if (_mqttClient.connected() != 0)
-        _mqttClient.stop();
-
-    if (_wifiClient.connected() != 0)
-        _wifiClient.stop();
-}
-
-//----------------------------------------------------------------------------
-bool spMQTTESP32::connect(void)
+bool spMQTTESP32Secure::connect(void)
 {
     // Already connected?
     if (connected())
         return true;
 
-    // NOTE: For future impl of secure clients connections
-    //       The Secure WiFi connection, when in InSecure() mode crashes
-    //       MQTT - so for now using a simple connection. Will debug 
-    //
-    // Is this a secure connection?
-    // if (caCertificate().length() == 0)
-    // {
-    //     Serial.println("DEBUG : MQTT - Insecure connection");
-    //     _wifiClient.setInsecure();
-    // }
-    // else
-    // {
-    //     _wifiClient.setCACert(caCertificate().c_str());
+    
+    if (caCertificate().length() == 0)
+        _wifiClient.setCACert(caCertificate().c_str());
 
-    //     if (clientCertificate().length() > 0)
-    //         _wifiClient.setCertificate(clientCertificate().c_str());
+    if (clientCertificate().length() > 0)
+        _wifiClient.setCertificate(clientCertificate().c_str());
 
-    //     if (clientKey().length() > 0)
-    //         _wifiClient.setPrivateKey(clientKey().c_str());
-    // }
+    if (clientKey().length() > 0)
+        _wifiClient.setPrivateKey(clientKey().c_str());
 
-    // do we have all the parameters we need?
-    if (clientName().length() == 0)
-    {
-        spLog_E(F("%s : No Thing/Client name set. Unable to connect"), name());
-        return false;
-    }
-    if (server().length() == 0)
-    {
-        spLog_E(F("%s : No server/endpoint set. Unable to connect"), name());
-        return false;
-    }
-    if (port() < 1024)
-    {
-        spLog_E(F("%s : A valid port is not set %d. Unable to connect"), name(), port());
-        return false;
-    }
+    return spMQTTESP32Base::connect();
 
-    // mqtt time
-
-    _mqttClient.setId(clientName().c_str());
-    _mqttClient.setKeepAliveInterval(60 * 1000);
-    _mqttClient.setConnectionTimeout(5 * 1000);
-
-   //  // Username/password provided?
-
-    if (username().length() > 0 && password().length() > 0)
-        _mqttClient.setUsernamePassword(username().c_str(), password().c_str());
-
-   //  // Connect
-   //  //Serial.printf("MQTT Connect: %s:%d\n\t", server().c_str(), port());
-
-    for( int i=0; !_mqttClient.connect(server().c_str(), port()); i++)
-    {
-        if (i > kMaxConnectionTries)
-        {
-            spLog_E(F("%s: MQTT connection failed. Error Code: %d"), name(), _mqttClient.connectError());
-            return false;
-        }
-        spLog_N_(".");
-        delay(kConnectionDelayMS);
-    }
-
-    // we're connected
-    return true;
+   
 }
 
-//----------------------------------------------------------------------------
 
-void spMQTTESP32::set_bufferSize(uint16_t buffSize)
-{
-    if (buffSize > 0)
-    {
-        _mqttClient.setTxPayloadSize(buffSize);
-        _dynamicBufferSize = buffSize;
-    }
-    _txBufferSize = buffSize;
-}
-//----------------------------------------------------------------------------
-uint16_t spMQTTESP32::get_bufferSize(void)
-{
-    return _txBufferSize;
-}
 
-//----------------------------------------------------------------------------
-// spWriter interface method
-void spMQTTESP32::write(const char * value, bool newline)
-{
-    // if we are not connected, ignore
-    if (!connected() || !value)
-        return;
-
-    // do we have a topic?
-    if ( topic().length() == 0 )
-    {
-        spLog_E(F("%s : No MQTT topic provided."), name());
-        return;
-    }
-
-    // the mqtt object has a limited transmitt buffer size (256) that doesn't adapt, 
-    // but you can set the size (which performs a malloc and free)
-    //
-    // Openlog payloads can be large, so if in dynamic mode we keep track of the 
-    // allocated size and increase when needed if in dynamic buffer size mode ..
-
-    if (_txBufferSize == 0 && _dynamicBufferSize < strlen(value))
-    {
-        _dynamicBufferSize = strlen(value);
-        _mqttClient.setTxPayloadSize(_dynamicBufferSize);
-    }
-
-    // send the message
-    _mqttClient.beginMessage(topic().c_str());
-    _mqttClient.print(value);
-    _mqttClient.endMessage();
-
-}
 #endif
--- src/Spark/spMQTTESP32.h
+++ src/Spark/spMQTTESP32.h
@@ -13,23 +13,77 @@
 #include <WiFiClientSecure.h>
 
 // A General MQTT client for the framework - for use on the ESP32
-
-class spMQTTESP32 : public spActionType<spMQTTESP32>, public spWriter
+template <class Object, typename CLIENT>
+class spMQTTESP32Base : public spActionType<Object>, public spWriter
 {
   private:
-    void set_isEnabled(bool bEnabled);
-    bool get_isEnabled(void);
 
-    void set_bufferSize(uint16_t);
-    uint16_t get_bufferSize(void);
+    // Enabled Property setter/getters
+    void set_isEnabled(bool bEnabled)
+    {
+        // Any changes?
+        if (_isEnabled == bEnabled)
+            return;
 
+        _isEnabled = bEnabled;
+
+        // TODO chech network connection availablity ...
+        if (_isEnabled)
+            (void)connect();
+        else
+            disconnect();
+    }
+
+    //----------------------------------------------------------------
+    bool get_isEnabled(void)
+    {
+        return _isEnabled;
+    }
+    //----------------------------------------------------------------------------
+
+    void set_bufferSize(uint16_t buffSize)
+    {
+        if (buffSize > 0)
+        {
+            _mqttClient.setTxPayloadSize(buffSize);
+            _dynamicBufferSize = buffSize;
+        }
+        _txBufferSize = buffSize;
+    }
+    //----------------------------------------------------------------------------
+    uint16_t get_bufferSize(void)
+    {
+        return _txBufferSize;
+    }
 
     // Event callback
     //----------------------------------------------------------------------------
-    void onConnectionChange(bool bConnected);
+    void onConnectionChange(bool bConnected)
+    {
+        // Are we enabled ...
+        if (!_isEnabled)
+            return;
+
+        // Anything change?
+        if (bConnected == connected())
+            return; 
+
+        if (bConnected)
+        {
+            spLog_I_(F("Connecting to MQTT endpoint %s:%u ..."), server().c_str(), port());
+            if (connect())
+                spLog_N(F("connected"));
+            // the connect method will print out sufficient error messages 
+        }
+        else
+        {
+            spLog_I(F("Disconnecting from MQTT endpoint %s"), clientName().c_str());
+            disconnect();
+        }
+    }
 
   public:
-    spMQTTESP32() : _isEnabled{true}, _theNetwork{nullptr}, _mqttClient(_wifiClient), 
+    spMQTTESP32Base() : _isEnabled{true}, _theNetwork{nullptr}, _mqttClient(_wifiClient), 
         _txBufferSize{0}, _dynamicBufferSize{0}
     {
         spRegister(enabled, "Enabled", "Enable or Disable the MQTT Client");
@@ -42,20 +96,14 @@ class spMQTTESP32 : public spActionType<spMQTTESP32>, public spWriter
         spRegister(username, "Username", "Username to connect to an MQTT broker, if required");
         spRegister(password, "Password", "Password to connect to an MQTT broker, if required");
 
-        spRegister(caCertificate, "CA Certificate", 
-                   "The Certificate Authority certificate. If set, the connection is secure");
-        spRegister(clientCertificate, "Client Certificate", "The certificate for the client connection");
-        spRegister(clientKey, "Client Key", "The secure key used for client verification");
-
-
         spRegister(bufferSize, "Buffer Size", "MQTT payload buffer size. If 0, the buffer size is dynamic");
 
-        setName("MQTT Client", "A MQTT Client for ESP32 Systems");
+        this->setName("MQTT Client", "A MQTT Client for ESP32 Systems");
 
         spark.add(this);
     };
 
-    ~spMQTTESP32()
+    ~spMQTTESP32Base()
     {
         disconnect();
         
@@ -65,7 +113,7 @@ class spMQTTESP32 : public spActionType<spMQTTESP32>, public spWriter
     void listenToConnection(spSignalBool &theEvent)
     {
         // Register to get notified on connection changes
-        theEvent.call(this, &spMQTTESP32::onConnectionChange);
+        theEvent.call(this, &spMQTTESP32Base::onConnectionChange);
     }
 
     void setNetwork(spNetwork *theNetwork)
@@ -74,11 +122,71 @@ class spMQTTESP32 : public spActionType<spMQTTESP32>, public spWriter
 
         listenToConnection(theNetwork->on_connectionChange);
     }
+    bool connected()
+    {
+        return (_isEnabled && _wifiClient.connected() != 0 && _mqttClient.connected() != 0);    
+    }
 
-    bool connect();
-    void disconnect();
+    //----------------------------------------------------------------------------
+    void disconnect(void)
+    {
+        if (_mqttClient.connected() != 0)
+            _mqttClient.stop();
 
-    bool connected();
+        if (_wifiClient.connected() != 0)
+            _wifiClient.stop();
+    }
+    //----------------------------------------------------------------------------
+    virtual bool connect(void)
+    {
+        // Already connected?
+        if (connected())
+            return true;
+
+
+        // do we have all the parameters we need?
+        if (clientName().length() == 0)
+        {
+            spLog_E(F("%s : No Thing/Client name set. Unable to connect"), this->name());
+            return false;
+        }
+        if (server().length() == 0)
+        {
+            spLog_E(F("%s : No server/endpoint set. Unable to connect"), this->name());
+            return false;
+        }
+        if (port() < 1024)
+        {
+            spLog_E(F("%s : A valid port is not set %d. Unable to connect"), this->name(), port());
+            return false;
+        }
+
+        // mqtt time
+        _mqttClient.setId(clientName().c_str());
+        _mqttClient.setKeepAliveInterval(60 * 1000);
+        _mqttClient.setConnectionTimeout(5 * 1000);
+
+        // Username/password provided?
+
+        if (username().length() > 0 && password().length() > 0)
+            _mqttClient.setUsernamePassword(username().c_str(), password().c_str());
+
+        // Connect
+
+        for( int i=0; !_mqttClient.connect(server().c_str(), port()); i++)
+        {
+            if (i > 4)
+            {
+                spLog_E(F("%s: MQTT connection failed. Error Code: %d"), this->name(), _mqttClient.connectError());
+                return false;
+            }
+            spLog_N_(".");
+            delay(400);
+        }
+
+        // we're connected
+        return true;
+    }
 
     // for the Writer interface
     void write(int data)
@@ -89,43 +197,96 @@ class spMQTTESP32 : public spActionType<spMQTTESP32>, public spWriter
     {
         //noop
     }
-    void write(const char *, bool);
+    //----------------------------------------------------------------------------
+    // spWriter interface method
+    void write(const char * value, bool newline)
+    {
+        // if we are not connected, ignore
+        if (!connected() || !value)
+            return;
+
+        // do we have a topic?
+        if ( topic().length() == 0 )
+        {
+            spLog_E(F("%s : No MQTT topic provided."), this->name());
+            return;
+        }
+
+        // the mqtt object has a limited transmitt buffer size (256) that doesn't adapt, 
+        // but you can set the size (which performs a malloc and free)
+        //
+        // Openlog payloads can be large, so if in dynamic mode we keep track of the 
+        // allocated size and increase when needed if in dynamic buffer size mode ..
+
+        if (_txBufferSize == 0 && _dynamicBufferSize < strlen(value))
+        {
+            _dynamicBufferSize = strlen(value);
+            _mqttClient.setTxPayloadSize(_dynamicBufferSize);
+        }
+
+        // send the message
+        _mqttClient.beginMessage(topic().c_str());
+        _mqttClient.print(value);
+        _mqttClient.endMessage();
+
+    }  
     // Properties
 
     // Enabled/Disabled
-    spPropertyRWBool<spMQTTESP32, &spMQTTESP32::get_isEnabled, &spMQTTESP32::set_isEnabled> enabled;
+    spPropertyRWBool<spMQTTESP32Base, &spMQTTESP32Base::get_isEnabled, &spMQTTESP32Base::set_isEnabled> enabled;
 
-    spPropertyUint<spMQTTESP32> port = {1883}; // default mqtt port
-    spPropertyString<spMQTTESP32> server;
-    spPropertyString<spMQTTESP32> topic;
-    spPropertyString<spMQTTESP32> clientName;
+    spPropertyUint<spMQTTESP32Base> port = {1883}; // default mqtt port
+    spPropertyString<spMQTTESP32Base> server;
+    spPropertyString<spMQTTESP32Base> topic;
+    spPropertyString<spMQTTESP32Base> clientName;
 
     // Buffer size property
-    spPropertyRWUint16<spMQTTESP32, &spMQTTESP32::get_bufferSize, &spMQTTESP32::set_bufferSize> bufferSize = {0};
+    spPropertyRWUint16<spMQTTESP32Base, &spMQTTESP32Base::get_bufferSize, &spMQTTESP32Base::set_bufferSize> bufferSize = {0};
 
 
     // username and password properties - some brokers requires this 
-    spPropertyString<spMQTTESP32> username;
-    spPropertySecureString<spMQTTESP32> password;
+    spPropertyString<spMQTTESP32Base> username;
+    spPropertySecureString<spMQTTESP32Base> password;
 
-    // Security certs/keys
-    spPropertySecureString<spMQTTESP32> caCertificate;
-    spPropertySecureString<spMQTTESP32> clientCertificate;
-    spPropertySecureString<spMQTTESP32> clientKey;
+  protected:
+    CLIENT  _wifiClient;
 
   private:
     bool _isEnabled;
 
     spNetwork *_theNetwork;
 
-    //WiFiClientSecure _wifiClient;
-
-    WiFiClient  _wifiClient;
-
     MqttClient  _mqttClient;
 
     uint16_t    _txBufferSize;
     uint16_t    _dynamicBufferSize;
+};
+
+
+class spMQTTESP32: public spMQTTESP32Base<spMQTTESP32, WiFiClient>
+{};
+
+class spMQTTESP32Secure: public spMQTTESP32Base<spMQTTESP32Secure, WiFiClientSecure>
+{
+
+public:
+
+    spMQTTESP32Secure()
+    {
+        spRegister(caCertificate, "CA Certificate", 
+                   "The Certificate Authority certificate. If set, the connection is secure");
+        spRegister(clientCertificate, "Client Certificate", "The certificate for the client connection");
+        spRegister(clientKey, "Client Key", "The secure key used for client verification");
+
+        this->setName("MQTT Secure Client", "A MQTT Secure Client for ESP32 Systems");
+    }
+
+    bool connect(void);
+
+    // Security certs/keys
+    spPropertySecureString<spMQTTESP32Secure> caCertificate;
+    spPropertySecureString<spMQTTESP32Secure> clientCertificate;
+    spPropertySecureString<spMQTTESP32Secure> clientKey;
 };
 
 #endif
