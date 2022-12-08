be352b8c2f37cacdaecc45cd17944c331d99c5d0
Kirk Benell <kirk.benell@sparkfun.com>
2022-12-08 15:26:48 -0700

    now sets the value of the topic that is expected by aws IoT

--- src/Spark/spAWSIoT.h
+++ src/Spark/spAWSIoT.h
@@ -6,6 +6,9 @@
 #include "spMQTTESP32.h"
 
 #define kAWSUpdateShadowTemplate "{\"state\":{\"reported\":%s}}"
+#define kAWSUpdateShadowTopic "$aws/things/%s/shadow/update"
+
+
 // simple class to support AWS IoT
 
 class spAWSIoT : public spMQTTESP32Secure
@@ -20,5 +23,18 @@ public:
         char szBuffer[strlen(value) + sizeof(kAWSUpdateShadowTemplate)];
         snprintf(szBuffer, sizeof(szBuffer),  kAWSUpdateShadowTemplate, value);
         spMQTTESP32Secure::write(szBuffer, false);
+    }
+
+    bool initialize(void)
+    {
+
+        spMQTTESP32Secure::initialize();
+
+        char szBuffer[clientName().length() + sizeof(kAWSUpdateShadowTopic)];
+        snprintf(szBuffer, sizeof(szBuffer), kAWSUpdateShadowTopic, clientName().c_str());
+
+        topic = szBuffer;
+        
+        return true;
     }
 };
