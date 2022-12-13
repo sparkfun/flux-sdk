8137f8162e605bca920d8733500eb0cefe4c5b77
Kirk Benell <kirk.benell@sparkfun.com>
2022-12-12 20:26:48 -0700

    initial entry - not functional, but compiles

---
+++ src/Spark/spAzureIoT.h
@@ -0,0 +1,55 @@
+
+
+
+#pragma once
+
+#include "spMQTTESP32.h"
+
+//#define kAWSUpdateShadowTemplate "{\"state\":{\"reported\":%s}}"
+//#define kAWSUpdateShadowTopic "$aws/things/%s/shadow/update"
+
+
+// simple class to support AWS IoT
+
+class spAzureIoT : public spMQTTESP32SecureCore<spAzureIoT>, public spWriter
+{
+public:
+    spAzureIoT()
+    {
+        setName("Azure IoT", "Connection to Azure IoT");
+        spark.add(this);
+    }
+    
+    // for the Writer interface
+    void write(int data)
+    {
+        // noop
+    }
+    void write(float data)
+    {
+        // noop
+    }
+    virtual void write(const char * value, bool newline)
+    {
+    	if (!value)
+    		return;
+
+    	// // Wrap the value with the structure required to update the device shadow
+        // char szBuffer[strlen(value) + sizeof(kAWSUpdateShadowTemplate)];
+        // snprintf(szBuffer, sizeof(szBuffer),  kAWSUpdateShadowTemplate, value);
+        // spMQTTESP32SecureCore::write(szBuffer, false);
+    }
+
+    bool initialize(void)
+    {
+
+        spMQTTESP32SecureCore::initialize();
+
+        // char szBuffer[clientName().length() + sizeof(kAWSUpdateShadowTopic)];
+        // snprintf(szBuffer, sizeof(szBuffer), kAWSUpdateShadowTopic, clientName().c_str());
+
+        // topic = szBuffer;
+        
+        return true;
+    }
+};
