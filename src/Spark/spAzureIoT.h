55e9cbba50e63a5a1dbaa0e3fe69ba5a057b0dbe
Kirk Benell <kirk.benell@sparkfun.com>
2022-12-15 18:37:28 -0700

    basic device connection now works: uses SAS auth, not X501 certs yet; SAS time refresh not implemented; data is sent as json *events*

--- src/Spark/spAzureIoT.h
+++ src/Spark/spAzureIoT.h
@@ -1,25 +1,67 @@
 
 
-
 #pragma once
 
 #include "spMQTTESP32.h"
 
-//#define kAWSUpdateShadowTemplate "{\"state\":{\"reported\":%s}}"
-//#define kAWSUpdateShadowTopic "$aws/things/%s/shadow/update"
+#include <az_core.h>
+#include <az_iot.h>
+#include <cstdlib>
+#include <string.h>
+#include <time.h>
+
+////////////////////////////////////////////////////////////////////////////////////////////
+// NOTE:
+//   This object requires the installation of the Azure SDK for C Arduino library
+//
+////////////////////////////////////////////////////////////////////////////////////////////
+
+// For initial operation 
+#define AZURE_SDK_CLIENT_USER_AGENT "c%2F" AZ_SDK_VERSION_STRING "(ard;sparkfun-esp32)"
+
+// This is copied from examples in the Azure C SDK. An default constructor and initialize() method were
+// added to allow post creation setup of the object. 
+
+class AzIoTSasToken
+{
+  public:
+    AzIoTSasToken() : sasToken{AZ_SPAN_EMPTY}
+    {
+    }
+
+    AzIoTSasToken(az_iot_hub_client *client, az_span deviceKey, az_span signatureBuffer, az_span sasTokenBuffer);
+    void initialize(az_iot_hub_client *client, az_span deviceKey, az_span signatureBuffer, az_span sasTokenBuffer);
+    int Generate(unsigned int expiryTimeInMinutes);
+    bool IsExpired();
+    az_span Get();
+
+  private:
+    az_iot_hub_client *client;
+    az_span deviceKey;
+    az_span signatureBuffer;
+    az_span sasTokenBuffer;
+    az_span sasToken;
+    uint32_t expirationUnixTime;
+};
 
 
-// simple class to support AWS IoT
+
+//---------------------------------------------------------------------    
+// simple class to support Azure IoT
 
 class spAzureIoT : public spMQTTESP32SecureCore<spAzureIoT>, public spWriter
 {
-public:
-    spAzureIoT()
+  public:
+    spAzureIoT() : _initialized{false}, _connected{false}
     {
         setName("Azure IoT", "Connection to Azure IoT");
+
+        spRegister(deviceID, "Device ID", "The device id for the Azure IoT device");
+        spRegister(deviceKey, "Device Key", "The device key for the Azure IoT device");
+
         spark.add(this);
     }
-    
+
     // for the Writer interface
     void write(int data)
     {
@@ -29,27 +71,165 @@ public:
     {
         // noop
     }
-    virtual void write(const char * value, bool newline)
+    //---------------------------------------------------------------------    
+    virtual void write(const char *value, bool newline)
     {
-    	if (!value)
-    		return;
+        if (!value || !_connected)
+            return;
 
-    	// // Wrap the value with the structure required to update the device shadow
-        // char szBuffer[strlen(value) + sizeof(kAWSUpdateShadowTemplate)];
-        // snprintf(szBuffer, sizeof(szBuffer),  kAWSUpdateShadowTemplate, value);
-        // spMQTTESP32SecureCore::write(szBuffer, false);
+        spMQTTESP32SecureCore::write(value, false);
     }
+
+    //---------------------------------------------------------------------
+    // Method mostly copyied this method from examples in the Azure SDK for C.
+    bool initializeIoTHubClient()
+    {
+
+        az_iot_hub_client_options options = az_iot_hub_client_options_default();
+        options.user_agent = AZ_SPAN_FROM_STR(AZURE_SDK_CLIENT_USER_AGENT);
+
+        // need to move hostname to a persistant char array from std::string. The called function
+        // assumes the value is persistant
+        strlcpy(_az_host, server().c_str(), sizeof(_az_host));
+        strlcpy(_az_device_id, deviceID().c_str(), sizeof(_az_device_id));
+
+        if (az_result_failed(az_iot_hub_client_init(&_client, az_span_create((uint8_t *)_az_host, strlen(_az_host)),
+                                                    az_span_create((uint8_t *)_az_device_id, strlen(_az_device_id)),
+                                                    &options)))
+        {
+            spLog_E(F("%s: Failed initializing Azure IoT Hub client"), name());
+            return false;
+        }
+
+        size_t client_id_length;
+        char mqtt_client_id[kBufferSize];
+        if (az_result_failed(az_iot_hub_client_get_client_id(&_client, mqtt_client_id, sizeof(mqtt_client_id) - 1,
+                                                             &client_id_length)))
+        {
+            spLog_E(F("%s: Failed getting client id"), name());
+            return false;
+        }
+
+        char mqtt_username[kBufferSize];
+        if (az_result_failed(az_iot_hub_client_get_user_name(&_client, mqtt_username, 
+                        sizeof(mqtt_username)/sizeof(mqtt_client_id[0]), NULL)))
+        {
+            spLog_E(F("%s: Failed to get azure username."),name());
+            return false;
+        }
+
+        // Update our properties with connected info
+        username = mqtt_username;
+        clientName = mqtt_client_id;
+
+        return true;
+    }
+
+    //---------------------------------------------------------------------
+    // override the connect method so that connection information can be accessed
+    // from the Azure SDK
+
+    bool connect()
+    {
+        // Do we need intializing
+        if (!_initialized)
+        {
+            if (!initialize())
+            {
+                spLog_E(F("%s failed to initialize"), name());
+                return false;
+            }
+        }
+
+        // Connect to the Azure IoT Hub
+        if (!initializeIoTHubClient())
+            return false;
+
+        if (_sasToken.Generate(60) != 0)
+        {
+            spLog_E(F("%s : Failed generating SAS authentication token"), name());
+            return false;
+        }
+
+        // Get the token/Password
+        char *token = (char *)az_span_ptr(_sasToken.Get());
+        if (!token)
+        {
+            spLog_E(F("%s: Error creating password token"), name());
+            return false;
+        }
+        // set the password property to the token
+        password = token;
+
+        // If we are here, we can get our mqtt update topic
+
+        char telemetry_topic[kBufferSize];
+
+        if (az_result_failed(az_iot_hub_client_telemetry_get_publish_topic(&_client, NULL, telemetry_topic,
+                                                                           sizeof(telemetry_topic), NULL)))
+        {
+            spLog_E(F("%s: Failed to retrieve azure mqtt topic from Azure SDK"), name());
+            return false;
+        }
+        // Set the topic property
+        topic = telemetry_topic;
+
+        _connected = spMQTTESP32SecureCore<spAzureIoT>::connect();
+
+        return _connected;
+    }
+
+    //---------------------------------------------------------------------
+    // Initialize method for the class
 
     bool initialize(void)
     {
+        if (_initialized)
+            return true;
 
-        spMQTTESP32SecureCore::initialize();
+        // do we have a device key
+        if (deviceKey().length() < 10)
+        {
+            spLog_E(F("%s: No device authentication key provided"), name());
+            return false;
+        }
 
-        // char szBuffer[clientName().length() + sizeof(kAWSUpdateShadowTopic)];
-        // snprintf(szBuffer, sizeof(szBuffer), kAWSUpdateShadowTopic, clientName().c_str());
+        if (!spMQTTESP32SecureCore::initialize())
+            return false;
 
-        // topic = szBuffer;
-        
+        // We need the key in persistant storage
+        strlcpy(_az_device_key, deviceKey().c_str(), sizeof(_az_device_key));
+
+        _sasToken.initialize(&_client, 
+                az_span_create((uint8_t*)_az_device_key, strlen(_az_device_key)),
+                az_span_create((uint8_t*)_sas_signature_buffer, sizeof(_sas_signature_buffer)),
+                az_span_create((uint8_t*)_mqtt_password, sizeof(_mqtt_password)));                
+
+        _initialized = true;
         return true;
     }
+
+    spPropertyString<spAzureIoT> deviceID;
+    spPropertySecureString<spAzureIoT> deviceKey;
+
+private:
+    static constexpr uint kBufferSize = 128;
+    static constexpr uint kBufferSize2X = kBufferSize * 2;
+    // The azure C SDK expects many of the passed in values to be persistant values, but
+    // our props are std::strings, which are dynamic. So create buffers in this object
+    // to manage the connection data for the SDK.
+
+    char _mqtt_password[kBufferSize2X];
+    uint8_t _sas_signature_buffer[kBufferSize2X];
+
+    az_iot_hub_client _client;
+
+    char _az_device_id[kBufferSize];    // we need persistant storage for the device id
+    char _az_host[kBufferSize];         // we need persistant, char array, storage for this
+    char _az_device_key[kBufferSize];    // "" "" "" 
+
+    bool _initialized;
+    bool _connected;
+
+    AzIoTSasToken _sasToken;
 };
