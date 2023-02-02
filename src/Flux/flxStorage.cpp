d745732711ebf620958e7ffab96f32a2b0a461aa
Kirk D. Benell <kirk.benell@sparkfun.com>
2023-02-02 16:08:47 -0700

    make use of the app token to encrypt strings

--- src/Flux/flxStorage.cpp
+++ src/Flux/flxStorage.cpp
@@ -19,13 +19,21 @@
 #include "flxCoreLog.h"
 #include "flxFlux.h"
 
-// TODO FIX
-char * key = "1234567kjueswabcoiu223smzx2he1as";
 
-bool flxStorageBlock::saveSecureString(flxStorageBlock *stBlk, const char * tag, char * data)
+bool flxStorageBlock::saveSecureString(const char * tag, const char * data)
 {
-    unsigned char IV[16];
 
+	if (!data || strlen(data) == 0)
+		return true;
+
+    // token/key
+    uint8_t token[32];
+    if (!flux.getAppToken(token))
+    {
+        flxLog_E(F("No app token set - unable to secure storage"));
+        return false;
+    }
+    unsigned char IV[16];
     memcpy(IV, flux.deviceId(), sizeof(IV));
 
     int len = strlen(data);
@@ -41,25 +49,35 @@ bool flxStorageBlock::saveSecureString(flxStorageBlock *stBlk, const char * tag,
     char encoded_buffer[buffer_size+1];
     memset(encoded_buffer, '\0', sizeof(encoded_buffer));
 
-    if (!flx_utils::encode_data_aes((uint8_t*)key, IV, input_buffer, encoded_buffer, buffer_size) )
+    if (!flx_utils::encode_data_aes(token, IV, input_buffer, encoded_buffer, buffer_size) )
     {
         flxLog_W("Error encoding secure string");
         return false;
     }
 
-   	bool status = stBlk->writeBytes(tag, (const uint8_t*)encoded_buffer, buffer_size);
+   	bool status = this->writeBytes(tag, (const uint8_t*)encoded_buffer, buffer_size);
     if (!status)
        flxLog_E("Error saving secure string for property: %s",  tag);
 
    return status;
 }
 
-bool flxStorageBlock::restoreSecureString(flxStorageBlock *stBlk, const char * tag, char * data, size_t len)
+bool flxStorageBlock::restoreSecureString(const char * tag, char * data, size_t len)
 {
 
+	if (!data || len == 0)
+		return false;
+	
+    // token/key
+    uint8_t token[32];
+    if (!flux.getAppToken(token))
+    {
+        flxLog_E(F("No app token set - unable to secure storage"));
+        return false;
+    }
 	char data_buffer[512];
 
-	size_t read_size = stBlk->readBytes(tag, (uint8_t*)data_buffer, sizeof(data_buffer));
+	size_t read_size = this->readBytes(tag, (uint8_t*)data_buffer, sizeof(data_buffer));
 	if (read_size == 0)
 		return false;
 
@@ -76,7 +94,7 @@ bool flxStorageBlock::restoreSecureString(flxStorageBlock *stBlk, const char * t
     unsigned char IV[16];
     memcpy(IV, flux.deviceId(), sizeof(IV));
 
-    if (!flx_utils::decode_data_aes((uint8_t*)key, IV, input_buffer, decoded_buffer, buffer_size) )
+    if (!flx_utils::decode_data_aes(token, IV, input_buffer, decoded_buffer, buffer_size) )
     {
         flxLog_W("Error decoding secure string");
         return false;
