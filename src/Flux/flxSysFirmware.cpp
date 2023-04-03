47e1b6bf3918f194e7da5562921b50a9525c0030
Kirk Benell <kirk.benell@sparkfun.com>
2023-04-03 15:42:46 -0600

    fix++ output for reboot message

--- src/Flux/flxSysFirmware.cpp
+++ src/Flux/flxSysFirmware.cpp
@@ -25,6 +25,9 @@
 void flxSysFirmware::doFactoryReset(void)
 {
 
+    flxLog_N("\n\r");
+    flxLog_I_("Performing Factory Reset...");
+
     // do we have a factory partiion on the device?
     esp_partition_iterator_t par_it = esp_partition_find(
             ESP_PARTITION_TYPE_APP, ESP_PARTITION_SUBTYPE_APP_FACTORY, NULL);
@@ -32,26 +35,32 @@ void flxSysFirmware::doFactoryReset(void)
     // Factory partition exist?
     if (par_it == NULL)
     {
-        flxLog_E(F("Factory Installed firmware not installed. Unable to perform a factory reset on the device"));
+        flxLog_N(F("Failed. Factory Installed firmware not installed."));
         return;
     }
+    flxLog_N_(".");
+
     const esp_partition_t *factory = esp_partition_get(par_it);
     esp_partition_iterator_release(par_it);
     esp_err_t  err = esp_ota_set_boot_partition(factory);
 
+    flxLog_N_(".");
+    
     if (err != ESP_OK)
     {
-        flxLog_E(F("Error setting factory firmware bootable. Unable to perform a factory reset on the device"));
+        flxLog_N(F("Failed. Error setting factory firmware bootable."));
         return;
     }
     // Firmware boot set to the factory partition, now reset the NVS
 
     // call low level ESP IDF functions to do this - as recommended by ESP32 docs ...
     nvs_flash_erase();
+    flxLog_N_(".");
     nvs_flash_init();
 
+
     // Now 
-    flxLog_I(F("\nFactory reset complete - rebooting..."));
+    flxLog_N(F("complete - rebooting..."));
 
     delay(500);
 
