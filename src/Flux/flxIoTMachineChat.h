bf5e68e211bf07e8c3a58713916f3d65e609a901
Kirk Benell <kirk.benell@sparkfun.com>
2023-04-19 15:17:38 -0600

    initial take of a MachineChat server connection

---
+++ src/Flux/flxIoTMachineChat.h
@@ -0,0 +1,74 @@
+/*
+ *---------------------------------------------------------------------------------
+ *
+ * Copyright (c) 2022-2023, SparkFun Electronics Inc.  All rights reserved.
+ * This software includes information which is proprietary to and a
+ * trade secret of SparkFun Electronics Inc.  It is not to be disclosed
+ * to anyone outside of this organization. Reproduction by any means
+ * whatsoever is  prohibited without express written permission.
+ *
+ *---------------------------------------------------------------------------------
+ */
+
+#pragma once
+
+#include "flxIoTHTTP.h"
+
+#include "flxFlux.h"
+#include "flxFmtJSON.h"
+#include "flxUtils.h"
+
+#define kOutputBufferSize 2500
+
+// simple class to support MachineChat output
+
+class flxIoTMachineChat : public flxIoTHTTPBase<flxIoTMachineChat>, public flxIWriterJSON
+{
+  public:
+    flxIoTMachineChat()
+    {
+        setName("MachineChat", "Connection to MachineChat");
+
+        flux.add(this);
+    }
+
+    void write(JsonDocument &jsonDoc)
+    {
+
+        if (!enabled())
+            return;
+
+        // no URL, no dice
+        if (URL().length() == 0)
+        {
+            flxLog_E(F("No URL provided for the MachineChat Server"));
+            return;
+        }
+
+        StaticJsonDocument<kOutputBufferSize> jsonOutput;
+
+        JsonObject jsonContext;
+        JsonObject jObj = jsonDoc.as<JsonObject>();
+
+        std::string strOutput;
+
+        for (JsonPair kv : jObj)
+        {
+            if (!kv.value().is<JsonObject>())
+                continue;
+
+            // okay, we have a sensor reading.
+            jsonOutput.clear();
+            jsonContext = jsonOutput.createNestedObject("context");
+            jsonContext["target_id"] = kv.key();
+
+            jsonOutput["data"] = kv.value();
+
+            strOutput.clear();
+            serializeJson(jsonOutput, strOutput);
+
+            // post to machine chat.
+            flxIoTHTTPBase<flxIoTMachineChat>::write(strOutput.c_str(), false);
+        }
+    }
+};
