469fce2b8e9aa35694fd98b0a46b1e618bb92fdc
Alex Brudner <101155592+SFE-Brudnerd@users.noreply.github.com>
2023-08-11 14:49:32 -0600

    Create initial files.

---
+++ src/Flux/flxDevBMI270.h
@@ -0,0 +1,31 @@
+/*
+ *---------------------------------------------------------------------------------
+ *
+ * Copyright (c) 2022-2023, SparkFun Electronics Inc.  All rights reserved.
+ * This software includes information which is proprietary to and a
+ * trade secret of SparkFun Electronics Inc.  It is not to be disclosed
+ * to anyone outside of this organization. Reproduction by any means
+ * whatsoever is  prohibited without express written permission.
+ * 
+ *---------------------------------------------------------------------------------
+ */
+ 
+/*
+ *
+ *  flxDevBMI270.h
+ *
+ *  Device object for the BMI270 Qwiic device.
+ *
+ *
+ *
+ */
+
+#pragma once
+
+#include <Arduino.h>
+
+#include "flxDevice.h"
+#include "SparkFun_BMI270_Arduino_Library.h"
+
+// What is the name used to ID this device?
+#define kBMI270DeviceName "BMI270"
