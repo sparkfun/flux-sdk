0dec701797d1aa88f9ca0c785ca38861d39cc868
Kirk Benell <kirk.benell@sparkfun.com>
2023-08-24 14:49:53 -0600

    fixed issue with limited json doc buffer size; Increased default size; improved error handling/messaging; made size configurable outside to object

--- src/Flux/flxStorageJSONPref.h
+++ src/Flux/flxStorageJSONPref.h
@@ -6,10 +6,10 @@
  * trade secret of SparkFun Electronics Inc.  It is not to be disclosed
  * to anyone outside of this organization. Reproduction by any means
  * whatsoever is  prohibited without express written permission.
- * 
+ *
  *---------------------------------------------------------------------------------
  */
- 
+
 /*
  * flxStorage.h
  *
@@ -18,58 +18,61 @@
 
 #pragma once
 
-#include "flxStorage.h"
 #include "flxFS.h"
+#include "flxStorage.h"
 
 #include <Arduino.h>
 #include <ArduinoJson.h>
+
+#define kDefaultJsonDocumentSize 6400
 //------------------------------------------------------------------------------
-// Store prefs to a JSON file 
+// Store prefs to a JSON file
 
 class flxStorageJSONPref;
 
-class flxStorageJSONBlock : public flxStorageBlock {
+class flxStorageJSONBlock : public flxStorageBlock
+{
 
-public:
-    flxStorageJSONBlock(): _readOnly{false}{}
+  public:
+    flxStorageJSONBlock() : _readOnly{false}
+    {
+    }
 
+    bool writeBool(const char *tag, bool data);
+    bool writeInt8(const char *tag, int8_t data);
+    bool writeInt16(const char *tag, int16_t data);
+    bool writeInt32(const char *tag, int32_t data);
+    bool writeUInt8(const char *tag, uint8_t data);
+    bool writeUInt16(const char *tag, uint16_t data);
+    bool writeUInt32(const char *tag, uint32_t data);
+    bool writeFloat(const char *tag, float data);
+    bool writeDouble(const char *tag, double data);
+    bool writeString(const char *tag, const char *data);
+    bool writeBytes(const char *tag, const uint8_t *data, size_t len);
 
-
-    bool writeBool(const char* tag, bool data);
-    bool writeInt8(const char* tag, int8_t data);
-    bool writeInt16(const char* tag, int16_t data);
-    bool writeInt32(const char* tag, int32_t data);
-    bool writeUInt8(const char* tag, uint8_t data);
-    bool writeUInt16(const char* tag, uint16_t data);
-    bool writeUInt32(const char* tag, uint32_t data);
-    bool writeFloat(const char* tag, float data);
-    bool writeDouble(const char* tag, double data);
-    bool writeString(const char* tag, const char* data);
-     bool writeBytes(const char* tag, const uint8_t* data, size_t len); 
-
-    bool readBool(const char* tag, bool& value);
-    bool readInt8(const char* tag, int8_t& value);
-    bool readInt16(const char* tag, int16_t& value);
-    bool readInt32(const char* tag, int32_t& value);
-    bool readUInt8(const char* tag, uint8_t& value);
-    bool readUInt16(const char* tag, uint16_t& value);
-    bool readUInt32(const char* tag, uint32_t& value);
-    bool readFloat(const char* tag, float& value);
-    bool readDouble(const char* tag, double& value);
-    size_t readString(const char* tag, char* data, size_t len);
-    size_t readBytes(const char* tag, uint8_t* data, size_t len);
+    bool readBool(const char *tag, bool &value);
+    bool readInt8(const char *tag, int8_t &value);
+    bool readInt16(const char *tag, int16_t &value);
+    bool readInt32(const char *tag, int32_t &value);
+    bool readUInt8(const char *tag, uint8_t &value);
+    bool readUInt16(const char *tag, uint16_t &value);
+    bool readUInt32(const char *tag, uint32_t &value);
+    bool readFloat(const char *tag, float &value);
+    bool readDouble(const char *tag, double &value);
+    size_t readString(const char *tag, char *data, size_t len);
+    size_t readBytes(const char *tag, uint8_t *data, size_t len);
 
     size_t getStringLength(const char *tag);
     size_t getBytesLength(const char *tag);
 
-
-    bool valueExists(const char* tag);
+    bool valueExists(const char *tag);
 
     flxStorage::flxStorageKind_t kind(void)
     {
         return flxStorage::flxStorageKindExternal;
     }
-private:
+
+  private:
     friend flxStorageJSONPref;
 
     JsonObject _jSection;
@@ -83,7 +86,7 @@ private:
 
     void setReadOnly(bool readonly)
     {
-        _readOnly=readonly;
+        _readOnly = readonly;
     }
 };
 
@@ -92,10 +95,13 @@ private:
 //
 // Interface for a storage system to persist state of a system
 
-class flxStorageJSONPref : public flxStorage {
+class flxStorageJSONPref : public flxStorage
+{
 
-public:
-    flxStorageJSONPref() : _pDocument{nullptr}, _readOnly{false}, _fileSystem{nullptr}, _filename{""}
+  public:
+    flxStorageJSONPref()
+        : _pDocument{nullptr}, _readOnly{false}, _fileSystem{nullptr}, _filename{""},
+          _jsonDocSize{kDefaultJsonDocumentSize}
     {
         setName("JSON File", "Device setting storage using a JSON File");
     }
@@ -105,15 +111,15 @@ public:
         return flxStorage::flxStorageKindExternal;
     }
     // add begin, end stubs - the Esp32 prefs system doesn't required transaction brackets
-    bool begin(bool readonly=false);
+    bool begin(bool readonly = false);
 
     void end(void);
-    
-    // public methods to manage a block
-    flxStorageJSONBlock* beginBlock(const char* tag);
 
-    flxStorageJSONBlock* getBlock(const char* tag);
-    void endBlock(flxStorageBlock*);
+    // public methods to manage a block
+    flxStorageJSONBlock *beginBlock(const char *tag);
+
+    flxStorageJSONBlock *getBlock(const char *tag);
+    void endBlock(flxStorageBlock *);
 
     void resetStorage();
 
@@ -125,20 +131,31 @@ public:
         setFilename(strName);
     }
 
+    void setDocumentSize(size_t new_size)
+    {
+        // need a sane value
+        if (new_size > 100)
+            _jsonDocSize = new_size;
+    }
 
-private:
+    size_t documentSize(void)
+    {
+        return _jsonDocSize;
+    }
 
+  private:
     void checkName();
     // The block used to interface with the system
     flxStorageJSONBlock _theBlock;
 
-    // Pointer to the json document 
+    // Pointer to the json document
 
-    DynamicJsonDocument  * _pDocument;
+    DynamicJsonDocument *_pDocument;
 
     bool _readOnly;
 
-    flxIFileSystem * _fileSystem;
+    flxIFileSystem *_fileSystem;
     std::string _filename;
-};
 
+    size_t _jsonDocSize;
+};
