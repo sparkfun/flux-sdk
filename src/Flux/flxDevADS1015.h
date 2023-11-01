cfb6952f4a4e7fef1c4564512b90216249b16f9e
Kirk Benell <kirk.benell@sparkfun.com>
2023-11-01 17:08:19 -0600

    added the ablity to change output types; also fixed potential issue with device init - system defaults not set; ran through formatter

--- src/Flux/flxDevADS1015.h
+++ src/Flux/flxDevADS1015.h
@@ -6,7 +6,7 @@
  * trade secret of SparkFun Electronics Inc.  It is not to be disclosed
  * to anyone outside of this organization. Reproduction by any means
  * whatsoever is  prohibited without express written permission.
- * 
+ *
  *---------------------------------------------------------------------------------
  */
 
@@ -15,8 +15,8 @@
  *  flxDevADS1015.h
  *
  *  Spark Device object for the ADS1015 A/D converter device.
- * 
- * 
+ *
+ *
  */
 
 #pragma once
@@ -24,8 +24,6 @@
 #include "Arduino.h"
 #include "SparkFun_ADS1015_Arduino_Library.h"
 #include "flxDevice.h"
-
-
 
 #define kADS1015DeviceName "ADS1015"
 
@@ -52,15 +50,46 @@ class flxDevADS1015 : public flxDeviceI2CType<flxDevADS1015>, public ADS1015
 
     bool onInitialize(TwoWire &);
 
-private:
-    float read_single_0();
-    float read_single_1();
-    float read_single_2();
-    float read_single_3();
-    float read_differential_P0_N1();
-    float read_differential_P0_N3();
-    float read_differential_P1_N3();
-    float read_differential_P2_N3();
+  private:
+    // Simple type codes used internally
+    static constexpr uint8_t kADS1015DeviceFloat = 0x1;
+    static constexpr uint8_t kADS1015DeviceInt = 0x2;
+    static constexpr uint8_t kADS1015DeviceUnsigned = 0x3;
+
+    float readf_single_0();
+    float readf_single_1();
+    float readf_single_2();
+    float readf_single_3();
+    float readf_differential_P0_N1();
+    float readf_differential_P0_N3();
+    float readf_differential_P1_N3();
+    float readf_differential_P2_N3();
+
+    // Ints
+    int16_t readi_single_0();
+    int16_t readi_single_1();
+    int16_t readi_single_2();
+    int16_t readi_single_3();
+    int16_t readi_differential_P0_N1();
+    int16_t readi_differential_P0_N3();
+    int16_t readi_differential_P1_N3();
+    int16_t readi_differential_P2_N3();
+
+    // uints
+    uint16_t readu_single_0();
+    uint16_t readu_single_1();
+    uint16_t readu_single_2();
+    uint16_t readu_single_3();
+
+    // prop methods
+
+    uint8_t get_se_type(void);
+    void set_se_type(uint8_t);
+    uint8_t _seType;
+
+    uint8_t get_diff_type(void);
+    void set_diff_type(uint8_t);
+    uint8_t _diffType;
 
     uint16_t get_sample_rate();
     void set_sample_rate(uint16_t);
@@ -72,24 +101,57 @@ private:
 
     bool _begun = false;
 
-public:
-    flxPropertyRWUint16<flxDevADS1015, &flxDevADS1015::get_sample_rate, &flxDevADS1015::set_sample_rate> sampleRate
-     = { ADS1015_CONFIG_RATE_1600HZ, { { "128 Hz", ADS1015_CONFIG_RATE_128HZ }, { "250 Hz", ADS1015_CONFIG_RATE_250HZ },
-                                       { "490 Hz", ADS1015_CONFIG_RATE_490HZ }, { "920 Hz", ADS1015_CONFIG_RATE_920HZ },
-                                       { "1600 Hz", ADS1015_CONFIG_RATE_1600HZ }, { "2400 Hz", ADS1015_CONFIG_RATE_2400HZ },
-                                       { "3300 Hz", ADS1015_CONFIG_RATE_3300HZ } } };
-    flxPropertyRWUint16<flxDevADS1015, &flxDevADS1015::get_pga_gain, &flxDevADS1015::set_pga_gain> gain
-     = { ADS1015_CONFIG_PGA_2, { { "x2/3", ADS1015_CONFIG_PGA_TWOTHIRDS }, { "x1", ADS1015_CONFIG_PGA_1 },
-                                 { "x2", ADS1015_CONFIG_PGA_2 }, { "x4", ADS1015_CONFIG_PGA_4 },
-                                 { "x8", ADS1015_CONFIG_PGA_8 }, { "x16", ADS1015_CONFIG_PGA_16 } } };
+  public:
+    flxPropertyRWUint16<flxDevADS1015, &flxDevADS1015::get_sample_rate, &flxDevADS1015::set_sample_rate> sampleRate = {
+        ADS1015_CONFIG_RATE_1600HZ,
+        {{"128 Hz", ADS1015_CONFIG_RATE_128HZ},
+         {"250 Hz", ADS1015_CONFIG_RATE_250HZ},
+         {"490 Hz", ADS1015_CONFIG_RATE_490HZ},
+         {"920 Hz", ADS1015_CONFIG_RATE_920HZ},
+         {"1600 Hz", ADS1015_CONFIG_RATE_1600HZ},
+         {"2400 Hz", ADS1015_CONFIG_RATE_2400HZ},
+         {"3300 Hz", ADS1015_CONFIG_RATE_3300HZ}}};
+    flxPropertyRWUint16<flxDevADS1015, &flxDevADS1015::get_pga_gain, &flxDevADS1015::set_pga_gain> gain = {
+        ADS1015_CONFIG_PGA_2,
+        {{"x2/3", ADS1015_CONFIG_PGA_TWOTHIRDS},
+         {"x1", ADS1015_CONFIG_PGA_1},
+         {"x2", ADS1015_CONFIG_PGA_2},
+         {"x4", ADS1015_CONFIG_PGA_4},
+         {"x8", ADS1015_CONFIG_PGA_8},
+         {"x16", ADS1015_CONFIG_PGA_16}}};
+
+    flxPropertyRWUint8<flxDevADS1015, &flxDevADS1015::get_se_type, &flxDevADS1015::set_se_type> singleEndedType = {
+        kADS1015DeviceFloat,
+        {{"Float", kADS1015DeviceFloat}, {"Integer", kADS1015DeviceInt}, {"Unsigned", kADS1015DeviceUnsigned}}};
+
+    flxPropertyRWUint8<flxDevADS1015, &flxDevADS1015::get_diff_type, &flxDevADS1015::set_diff_type> differentialType = {
+        kADS1015DeviceFloat, {{"Float", kADS1015DeviceFloat}, {"Integer", kADS1015DeviceInt}}};
 
     // Define our output parameters - specify the get functions to call.
-    flxParameterOutFloat<flxDevADS1015, &flxDevADS1015::read_single_0> channel0;
-    flxParameterOutFloat<flxDevADS1015, &flxDevADS1015::read_single_1> channel1;
-    flxParameterOutFloat<flxDevADS1015, &flxDevADS1015::read_single_2> channel2;
-    flxParameterOutFloat<flxDevADS1015, &flxDevADS1015::read_single_3> channel3;
-    flxParameterOutFloat<flxDevADS1015, &flxDevADS1015::read_differential_P0_N1> differential_0_minus_1;
-    flxParameterOutFloat<flxDevADS1015, &flxDevADS1015::read_differential_P0_N3> differential_0_minus_3;
-    flxParameterOutFloat<flxDevADS1015, &flxDevADS1015::read_differential_P1_N3> differential_1_minus_3;
-    flxParameterOutFloat<flxDevADS1015, &flxDevADS1015::read_differential_P2_N3> differential_2_minus_3;
+
+    // Floats!
+    flxParameterOutFloat<flxDevADS1015, &flxDevADS1015::readf_single_0> channel0_f;
+    flxParameterOutFloat<flxDevADS1015, &flxDevADS1015::readf_single_1> channel1_f;
+    flxParameterOutFloat<flxDevADS1015, &flxDevADS1015::readf_single_2> channel2_f;
+    flxParameterOutFloat<flxDevADS1015, &flxDevADS1015::readf_single_3> channel3_f;
+    flxParameterOutFloat<flxDevADS1015, &flxDevADS1015::readf_differential_P0_N1> differential_0_minus_1_f;
+    flxParameterOutFloat<flxDevADS1015, &flxDevADS1015::readf_differential_P0_N3> differential_0_minus_3_f;
+    flxParameterOutFloat<flxDevADS1015, &flxDevADS1015::readf_differential_P1_N3> differential_1_minus_3_f;
+    flxParameterOutFloat<flxDevADS1015, &flxDevADS1015::readf_differential_P2_N3> differential_2_minus_3_f;
+
+    // ints!
+    flxParameterOutInt16<flxDevADS1015, &flxDevADS1015::readi_single_0> channel0_i;
+    flxParameterOutInt16<flxDevADS1015, &flxDevADS1015::readi_single_1> channel1_i;
+    flxParameterOutInt16<flxDevADS1015, &flxDevADS1015::readi_single_2> channel2_i;
+    flxParameterOutInt16<flxDevADS1015, &flxDevADS1015::readi_single_3> channel3_i;
+    flxParameterOutInt16<flxDevADS1015, &flxDevADS1015::readi_differential_P0_N1> differential_0_minus_1_i;
+    flxParameterOutInt16<flxDevADS1015, &flxDevADS1015::readi_differential_P0_N3> differential_0_minus_3_i;
+    flxParameterOutInt16<flxDevADS1015, &flxDevADS1015::readi_differential_P1_N3> differential_1_minus_3_i;
+    flxParameterOutInt16<flxDevADS1015, &flxDevADS1015::readi_differential_P2_N3> differential_2_minus_3_i;
+
+    // unsigned ints!
+    flxParameterOutUint16<flxDevADS1015, &flxDevADS1015::readu_single_0> channel0_u;
+    flxParameterOutUint16<flxDevADS1015, &flxDevADS1015::readu_single_1> channel1_u;
+    flxParameterOutUint16<flxDevADS1015, &flxDevADS1015::readu_single_2> channel2_u;
+    flxParameterOutUint16<flxDevADS1015, &flxDevADS1015::readu_single_3> channel3_u;
 };
