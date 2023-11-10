f4d8befbde5bb4f7a824594619a7454e07f5bfab
Kirk Benell <kirk.benell@sparkfun.com>
2023-11-10 11:08:03 -0700

    bug fix/improved logic for machine chat connection - based on input from machine chat

--- src/Flux/flxIoTMachineChat.h
+++ src/Flux/flxIoTMachineChat.h
@@ -18,21 +18,31 @@
 #include "flxFmtJSON.h"
 #include "flxUtils.h"
 
-#include <time.h>
 #include <string.h>
+#include <time.h>
 
-#define kOutputBufferSize 2500
+#define kOutputBufferSize 1600
 
 // simple class to support MachineChat output
 
 class flxIoTMachineChat : public flxIoTHTTPBase<flxIoTMachineChat>, public flxIWriterJSON
 {
   public:
-    flxIoTMachineChat()
+    flxIoTMachineChat() : _isInitalized{false}, _szLocalIP{""}
     {
         setName("MachineChat", "Connection to MachineChat");
 
         flux.add(this);
+    }
+
+    void setupTargetIP(void)
+    {
+        WiFiClient *myClient = getWiFiClient();
+        if (!myClient)
+            return;
+
+        IPAddress myIP = myClient->localIP();
+        snprintf(_szLocalIP, sizeof(_szLocalIP), "%u.%u.%u.%u", myIP[0], myIP[1], myIP[2], myIP[3]);
     }
 
     void write(JsonDocument &jsonDoc)
@@ -48,33 +58,58 @@ class flxIoTMachineChat : public flxIoTHTTPBase<flxIoTMachineChat>, public flxIW
             return;
         }
 
-        // We need time for the timestamp, TODO - make this a util function ...
-        char szBuffer[64];
-        memset(szBuffer, '\0', sizeof(szBuffer));
+        if (!_isInitalized)
+        {
+            setupTargetIP();
+            _isInitalized = true;
+        }
+
+        char szTime[64];
+        memset(szTime, '\0', sizeof(szTime));
 
         time_t t_now;
         time(&t_now);
-        flx_utils::timestampISO8601(t_now, szBuffer, sizeof(szBuffer), true);
+        flx_utils::timestampISO8601(t_now, szTime, sizeof(szTime), true);
 
-        StaticJsonDocument<kOutputBufferSize> jsonOutput;
+        DynamicJsonDocument jsonOutput(kOutputBufferSize);
 
         JsonObject jsonContext;
-        JsonObject jObj = jsonDoc.as<JsonObject>();
+        JsonObject jRoot = jsonDoc.as<JsonObject>();
 
         std::string strOutput;
 
-        for (JsonPair kv : jObj)
+        char szName[64];
+        char szData[64];
+
+        // loop over top level objects in the output document - these are devices
+        for (JsonPair kvObj : jRoot)
         {
-            if (!kv.value().is<JsonObject>())
+            if (!kvObj.value().is<JsonObject>())
                 continue;
 
-            // okay, we have a sensor reading.
-            jsonOutput.clear();
-            jsonContext = jsonOutput.createNestedObject("context");
-            jsonContext["target_id"] = kv.key();
-            jsonContext["timestamp"] = szBuffer;
+            // get a proper context name/ID --
+            if (!flx_utils::createVariableName(kvObj.key().c_str(), szName))
+            {
+                flxLog_E(F("%s: Unable to create context id for: %s"), name(), kvObj.key().c_str());
+                continue;
+            }
 
-            jsonOutput["data"] = kv.value();
+            jsonOutput.clear();
+            jsonOutput["context"]["target_id"] = szName;
+            jsonOutput["context"]["target_ip"] = _szLocalIP;
+            jsonOutput["context"]["timestamp"] = szTime;
+
+            // loop over data
+            for (JsonPair kvParam : kvObj.value().as<JsonObject>())
+            {
+                // cleanup the name
+                if (!flx_utils::createVariableName(kvParam.key().c_str(), szData))
+                {
+                    flxLog_E(F("%s: Unable to create data name for: %s"), name(), kvParam.key().c_str());
+                    continue;
+                }
+                jsonOutput["data"][szData] = kvParam.value();
+            }
 
             strOutput.clear();
             serializeJson(jsonOutput, strOutput);
@@ -83,4 +118,8 @@ class flxIoTMachineChat : public flxIoTHTTPBase<flxIoTMachineChat>, public flxIW
             flxIoTHTTPBase<flxIoTMachineChat>::write(strOutput.c_str(), false, flxLineTypeData);
         }
     }
+
+  private:
+    bool _isInitalized;
+    char _szLocalIP[16];
 };
