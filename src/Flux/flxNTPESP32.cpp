4fbccf7642294c98653d08f63336093762c68369
Kirk Benell <kirk.benell@sparkfun.com>
2023-12-12 10:59:21 -0700

    got ran through formatter; no functional change

--- src/Flux/flxNTPESP32.cpp
+++ src/Flux/flxNTPESP32.cpp
@@ -6,20 +6,17 @@
  * trade secret of SparkFun Electronics Inc.  It is not to be disclosed
  * to anyone outside of this organization. Reproduction by any means
  * whatsoever is  prohibited without express written permission.
- * 
+ *
  *---------------------------------------------------------------------------------
  */
- 
-
-
 
 #ifdef ESP32
 
 #include "flxNTPESP32.h"
 
-#include "time.h"
-#include "lwip/apps/sntp.h"
 #include "esp_netif.h"
+#include "lwip/apps/sntp.h"
+#include "time.h"
 
 //----------------------------------------------------------------
 // Enabled Property setter/getters
@@ -27,16 +24,16 @@ void flxNTPESP32::set_isEnabled(bool bEnabled)
 {
     // Any changes?
     if (_isEnabled == bEnabled)
-        return;  
+        return;
 
     // changing state ...
     // TODO - Should this control connection state?
 
     _isEnabled = bEnabled;
 
-    if ( _isEnabled)
+    if (_isEnabled)
         (void)start();
-    else 
+    else
         stop();
 }
 
@@ -46,13 +43,13 @@ bool flxNTPESP32::get_isEnabled(void)
     return _isEnabled;
 }
 
-// Event callback 
+// Event callback
 //---------------------------------------------------------------
 void flxNTPESP32::onConnectionChange(bool bConnected)
 {
     // Are we enabled ...
     if (!_isEnabled)
-        return; 
+        return;
 
     if (bConnected)
         start();
@@ -63,57 +60,56 @@ void flxNTPESP32::onConnectionChange(bool bConnected)
 bool flxNTPESP32::start(void)
 {
     // already running?
-    if(sntp_enabled())
+    if (sntp_enabled())
         return true;
 
     // do we have a network and is it connected
-    if ( !_theNetwork || !_theNetwork->isConnected())
+    if (!_theNetwork || !_theNetwork->isConnected())
     {
         flxLog_W(F("NTP Client - Unable to start, no network connection"));
         return false;
     }
-    
+
     // KDB - Nov 2023
-    // 
-    // We were using the helper method - configTZTime() in the Arudino ESP port, but
+    //
+    // We were using the helper method - configTZTime() in the Arduino ESP port, but
     // it set the Timezone and the ntp servers (and got everything going). But for Flux,
-    // the timezone is handled by the framework Clock object -- which would cause 
+    // the timezone is handled by the framework Clock object -- which would cause
     // a conflict -- either this object could grab the TZ from the clock, which connects
     // the two object together - not good design. Or just setup the NTP system, but skip
     // the Time zone work . This is what is done here.
     //
     // The following are from the implementation of configTzTime() - minus the TZ setup.
 
-
     esp_netif_init();
-    if(sntp_enabled()){
+    if (sntp_enabled())
+    {
         sntp_stop();
     }
     sntp_setoperatingmode(SNTP_OPMODE_POLL);
-    sntp_setservername(0, (char*)ntpServerOne().c_str());
-    sntp_setservername(1, (char*)ntpServerTwo().c_str());
+    sntp_setservername(0, (char *)ntpServerOne().c_str());
+    sntp_setservername(1, (char *)ntpServerTwo().c_str());
     sntp_init();
-    
+
     // end configTZtime() inline
 
-    // wait for time to sync if a delay is set. 
+    // wait for time to sync if a delay is set.
     // The indication that NTP is working is time moves to a current year
     if (_startupDelay)
     {
         uint32_t start = millis();
         uint32_t secs;
-        uint32_t inc=0;
+        uint32_t inc = 0;
 
         struct tm *tm_now;
         time_t now;
 
         flxLog_I_(F("NTP startup [%u secs]..."), _startupDelay);
 
-
         while (true)
         // while (millis() - start < _startupDelay*1000)
         {
-            secs = (millis() - start)/1000;
+            secs = (millis() - start) / 1000;
             if (secs >= _startupDelay)
                 break;
             time(&now);
@@ -142,18 +138,18 @@ bool flxNTPESP32::start(void)
 void flxNTPESP32::stop(void)
 {
     // If the sntp system is up, shut it down
-    if(sntp_enabled())
+    if (sntp_enabled())
         sntp_stop();
 }
 //----------------------------------------------------------
-// Clock interface things 
+// Clock interface things
 //----------------------------------------------------------
-uint flxNTPESP32::get_epoch(void){
+uint flxNTPESP32::get_epoch(void)
+{
 
     time_t now;
     time(&now);
     return now;
-
 }
 
 bool flxNTPESP32::valid_epoch(void)
