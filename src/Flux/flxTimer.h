235f5d110b269563230fa18117e762c64b6ec17b
Kirk Benell <kirk.benell@sparkfun.com>
2023-12-20 20:26:03 -0700

    remove loop, make use of job queue

--- src/Flux/flxTimer.h
+++ src/Flux/flxTimer.h
@@ -14,44 +14,59 @@
 
 #include <Arduino.h>
 
+#include "flxCoreJobs.h"
 #include "flxFlux.h"
 
 class flxTimer : public flxActionType<flxTimer>
 {
 
+    uint get_interval(void)
+    {
+        return _timerJob.period();
+    }
+
+    void set_interval(uint in_interval)
+    {
+        _timerJob.setPeriod(in_interval);
+    }
+
+    void onTimer(void)
+    {
+        on_interval.emit();
+    }
+
     // for our timing
     unsigned long _lastLogTime = 0;
+
+    flxJob _timerJob;
 
   public:
     flxTimer(int start = 500) : _lastLogTime(0)
     {
 
         flxRegister(interval, "Interval", "Timer interval in milliseconds");
-        interval = start;
+
+        // interval = start;
 
         flux.add(this);
 
         setName("Timer", "A reoccurring timer");
+
+        _timerJob.setPeriod(start);
+        _timerJob.setName("Timer");
     };
+
+    bool initialize(void)
+    {
+        _timerJob.setHandler(this, &flxTimer::onTimer);
+        flxJobQueue.addJob(_timerJob);
+
+        return true;
+    }
 
     // Our output event
     flxSignalVoid on_interval;
 
     // Property  - interval for the timer -- and set a default value of 10 seconds
-    flxPropertyUint<flxTimer> interval = {10000};
-
-    bool loop(void)
-    {
-        // Is it time to log - Delta since last log > Log Interval??
-        if (millis() - _lastLogTime < interval)
-            return false; // we didn't do anything
-
-        // Reset our last logged time to now
-        _lastLogTime = millis();
-
-        // trigger the event
-        on_interval.emit();
-
-        return true;
-    }
+    flxPropertyRWUint<flxTimer, &flxTimer::get_interval, &flxTimer::set_interval> interval = {10000};
 };
