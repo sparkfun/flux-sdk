a4c48db7e6201edd0bfa32bf87f47ed86750c29e
Kirk Benell <kirk.benell@sparkfun.com>
2023-12-27 07:51:00 -0700

    removed loop() -> now uses job queue; refactored to cleanup/simplifiy implementation - remove non-needed static buffers in object

--- src/Flux/flxDevST25DV.cpp
+++ src/Flux/flxDevST25DV.cpp
@@ -6,10 +6,10 @@
  * trade secret of SparkFun Electronics Inc.  It is not to be disclosed
  * to anyone outside of this organization. Reproduction by any means
  * whatsoever is  prohibited without express written permission.
- * 
+ *
  *---------------------------------------------------------------------------------
  */
- 
+
 /*
  *
  *  flxDevST25DV.cpp
@@ -31,6 +31,8 @@
 */
 uint8_t flxDevST25DV::defaultDeviceAddress[] = {0x57, kSparkDeviceAddressNull};
 
+const uint32_t kST25DVTimeOutPeriod = 15000;
+const uint32_t kST25DVFieldDetectPeriod = 5000;
 //----------------------------------------------------------------------------------------------------------
 // Register this class with the system, enabling this driver during system
 // initialization and device discovery.
@@ -42,7 +44,7 @@ flxRegisterDevice(flxDevST25DV);
 // Object constructor. Performs initialization of device values, including device identifiers (name, I2C address),
 // and managed properties.
 
-flxDevST25DV::flxDevST25DV()
+flxDevST25DV::flxDevST25DV() : _sPassword{""}, _sSSID{""}
 {
 
     // Setup unique identifiers for this device and basic device object systems
@@ -50,8 +52,11 @@ flxDevST25DV::flxDevST25DV()
     setDescription("ST25DV Dynamic RFID Tag");
 
     // Register Properties
-    flxRegister(ssid, "WiFi Record SSID", "The Service Set Identifier held in the WiFi record");
-    flxRegister(password, "WiFi Record Key / Password", "The key (password) held in the WiFi record");
+    flxRegister(ssid, "WiFi Record SSID", "The SSID held in the WiFi record");
+    flxRegister(password, "WiFi Record Key / Password", "The password held in the WiFi record");
+
+    // The update job used for this device - start with a 1 second timeout.
+    _theJob.setup("ST25DV Device", 1000, this, &flxDevST25DV::jobHandlerCB);
 }
 
 //----------------------------------------------------------------------------------------------------------
@@ -66,7 +71,7 @@ bool flxDevST25DV::isConnected(flxBusI2C &i2cDriver, uint8_t address)
     // Note: these values are different to the ones in the ST25DVxxKC datasheet!
     // We found 0x51 by reading the device, and in this App Note:
     // https://www.st.com/resource/en/application_note/an5675-st25dv64kcdisco-firmware-documentation-stmicroelectronics.pdf
-    uint8_t icRefReg[2] = { 0x00, 0x1D };
+    uint8_t icRefReg[2] = {0x00, 0x1D};
     if (!i2cDriver.write(address, icRefReg, 2))
         return false;
     uint8_t icRef = 0;
@@ -75,7 +80,7 @@ bool flxDevST25DV::isConnected(flxBusI2C &i2cDriver, uint8_t address)
 
     if ((icRef == 0x50) || (icRef == 0x51))
     {
-        //flxLog_I("ST25DV: isConnected icRef 0x%x", icRef);
+        // flxLog_I("ST25DV: isConnected icRef 0x%x", icRef);
         return true;
     }
     else
@@ -97,84 +102,87 @@ bool flxDevST25DV::onInitialize(TwoWire &wirePort)
     bool rc = SFE_ST25DV64KC_NDEF::begin(wirePort);
 
     if (!rc)
-        flxLog_E("ST25DV - begin failed");
+        flxLogM_E(kMsgErrDeviceInit, name(), "start");
+    else
+        flxAddJobToQueue(_theJob); // start our timer job
 
     return rc;
 }
 
-// methods for the read-write properties
-std::string flxDevST25DV::get_ssid()
-{
-    bool success = false;
-    if (!_ssid)
-    {
-        success = SFE_ST25DV64KC_NDEF::readNDEFWiFi(_readSsid, MAX_SSID_PASSWORD_LEN, _readPassword, MAX_SSID_PASSWORD_LEN); // Read the first WiFi Record
-    }
-    if (success)
-    {
-        _password = true;
-    }
-    _ssid = false;
-
-    std::string theString = _readSsid;
-
-    return theString;
-}
-void flxDevST25DV::set_ssid(std::string newSsid)
+//----------------------------------------------------------------------------------------------------------
+bool flxDevST25DV::updateWiFiRecord(void)
 {
 
-}
-std::string flxDevST25DV::get_password()
-{
-    bool success = false;
-    if (!_password)
-    {
-        success = SFE_ST25DV64KC_NDEF::readNDEFWiFi(_readSsid, MAX_SSID_PASSWORD_LEN, _readPassword, MAX_SSID_PASSWORD_LEN); // Read the first WiFi Record
-    }
-    if (success)
-    {
-        _ssid = true;
-    }
-    _password = false;
+    char szPassword[kRecordElementLength];
+    char szSSID[kRecordElementLength];
 
-    std::string theString = _readPassword;
+    bool status = SFE_ST25DV64KC_NDEF::readNDEFWiFi(szSSID, kRecordElementLength, (char *)szPassword,
+                                                    kRecordElementLength); // Read the first WiFi Record
 
-    return theString;
-}
-void flxDevST25DV::set_password(std::string newPassword)
-{
-    
+    if (!status)
+        return false;
+
+    // Anything change?
+    status = _sPassword.compare(szPassword) != 0 || _sSSID.compare(szSSID) != 0;
+
+    if (status)
+    {
+        _sPassword = szPassword;
+        _sSSID = szSSID;
+    }
+    return status;
 }
 
 //----------------------------------------------------------------------------------------------------------
-// Loop
-
-bool flxDevST25DV::loop(void)
+// methods for the read-write properties
+std::string flxDevST25DV::get_ssid()
 {
-    bool result = false;
-
-    static unsigned long lastMillis = millis() - 14000; // Read the tag after 1 second, first time around
-
-    if (millis() > (lastMillis + 15000)) // Check tag every 15 seconds to avoid possible I2C/RF collisions
+    if (_sSSID.length() == 0)
     {
-        if (SFE_ST25DV64KC_NDEF::RFFieldDetected()) // Do a quick RF check first
-        {
-            lastMillis = millis() - 10000; // Wait another 5 seconds if an RF field was detected
-        }
-        else
-        {
-            lastMillis = millis(); // Update lastMillis
-            bool success = SFE_ST25DV64KC_NDEF::readNDEFWiFi(_readSsid, MAX_SSID_PASSWORD_LEN, _readPassword, MAX_SSID_PASSWORD_LEN); // Read the first WiFi Record
-            if (success && (strlen(_readSsid) > 0) && (strlen(_readPassword) > 0)
-                && ((strcmp(_readSsid, _previousSsid) != 0) || (strcmp(_readPassword, _previousPassword) != 0))) // If either the SSID or Password has changed
-            {
-                strcpy(_previousPassword, _readPassword); // Update the previous SSID and Password
-                strcpy(_previousSsid, _readSsid);
-                new_WiFi_record.emit();
-                result = true;
-            }
-        }
+        // any updates available?
+        updateWiFiRecord();
+    }
+    return _sSSID;
+}
+//----------------------------------------------------------------------------------------------------------
+void flxDevST25DV::set_ssid(std::string newSsid)
+{
+}
+
+//----------------------------------------------------------------------------------------------------------
+std::string flxDevST25DV::get_password()
+{
+
+    if (_sPassword.length() == 0)
+    {
+        // Any updates available?
+        updateWiFiRecord();
     }
 
-    return result;
+    return _sPassword;
+}
+//----------------------------------------------------------------------------------------------------------
+//
+void flxDevST25DV::set_password(std::string newPassword)
+{
+}
+
+//----------------------------------------------------------------------------------------------------------
+// Timed job callback handler
+
+void flxDevST25DV::jobHandlerCB(void)
+{
+
+    // update the jobs period - give the device 5 seconds to try to detect?
+    if (SFE_ST25DV64KC_NDEF::RFFieldDetected()) // Do a quick RF check first
+        _theJob.setPeriod(kST25DVFieldDetectPeriod);
+    else
+    {
+        if (updateWiFiRecord())
+            new_WiFi_record.emit();
+
+        // tweak the period if needed - back to normal check level after startup
+        if (_theJob.period() != kST25DVTimeOutPeriod)
+            _theJob.setPeriod(kST25DVTimeOutPeriod);
+    }
 }
